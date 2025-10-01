2b21dfe255a7d5e32e7bc58c80d810f370119c69
Olena Borysonok <olena.borysonok@gmail.com>
2025-10-01 19:23:03 -0400

    FirstTestDemoQa

---
+++ src/test/java/school/redrover/LenaB.java
@@ -0,0 +1,33 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LenaB {
+    @Test
+    public void testFullNameBox(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://demoqa.com/text-box");
+        System.out.println(driver.getTitle());
+
+        WebElement inputBox = driver.findElement(By.xpath("//*[@id=\"userName\"]"));
+        inputBox.sendKeys("Olena Name");
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button#submit"));
+        submitButton.click();
+
+
+        WebElement testOutputBox = driver.findElement(By.xpath("//*[@id=\"name\"]"));
+
+        Assert.assertEquals(testOutputBox.getText(), "Name:Olena Name");
+        driver.quit();
+
+    }
+
+}
