54641296056bacb72f95683a64dd1615975d2616
Akiko <72522955+Akiko-QA@users.noreply.github.com>
2025-10-02 16:27:59 +0400

    add first test (#9)

---
+++ src/test/java/school/redrover/AkikoTest.java
@@ -0,0 +1,30 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AkikoTest {
+
+    public static final String LOGIN = "admin@admin.com";
+    public static final String PASSWORD = "admin123";
+
+    @Test
+    public void testPositiveLogin() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://qa-practice.netlify.app/auth_ecommerce");
+
+        driver.findElement(By.name("emailAddress")).sendKeys(LOGIN);
+        driver.findElement(By.name("password")).sendKeys(PASSWORD);
+        driver.findElement(By.id("submitLoginBtn")).click();
+
+        Assert.assertTrue(driver.findElement(By.id("logout")).isDisplayed(),
+                "Logout button should be displayed after successful login");
+
+        driver.close();
+    }
+}
