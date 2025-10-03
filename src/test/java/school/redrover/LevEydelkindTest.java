3dd38a1965731dace5433b41c1f72b4253d81240
Lev Eydelkind <eydelkindl@gmail.com>
2025-10-03 16:08:19 +0300

    Method sauceDemo changed

--- src/test/java/school/redrover/LevEydelkindTest.java
+++ src/test/java/school/redrover/LevEydelkindTest.java
@@ -1,5 +1,30 @@
 package school.redrover;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class LevEydelkindTest {
 
+    @Test
+    public void sauceDemo (){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com");
+        driver.manage().window().maximize();
+
+        //driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl, "https://www.saucedemo.com/inventory.html");
+
+        driver.quit();
+
+    }
 }
