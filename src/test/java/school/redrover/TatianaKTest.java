279c420e6129bc3b1fd7febba6748641fc08c912
KobusTA25 <125394170+KobusTA25@users.noreply.github.com>
2025-10-05 11:18:57 +0300

    TatianaKTest add

---
+++ src/test/java/school/redrover/TatianaKTest.java
@@ -0,0 +1,36 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TatianaKTest {
+    @Test
+    public void testSelenium() {
+        WebDriver driver = new ChromeDriver();
+
+
+        driver.get("https://practice-automation.com/modals/");
+
+        WebElement simpleModalButton = driver.findElement(By.xpath("//*[@id=\"formModal\"]"));
+        simpleModalButton.click();
+
+        WebElement textName = driver.findElement(By.xpath("//*[@id=\"g1051-name\"]"));
+        textName.sendKeys("Tatiana");
+
+        WebElement textEmail = driver.findElement(By.xpath("//*[@id=\"g1051-email\"]"));
+        textEmail.sendKeys("Tatiana@gmail.com");
+
+        WebElement textMessage = driver.findElement(By.xpath("//*[@id=\"contact-form-comment-g1051-message\"]"));
+        textMessage.sendKeys("Hi! How are you?");
+
+        WebElement submitButton = driver.findElement(By.xpath("//*[@id=\"jp-form-99ddf6d0cf76daf0c77607b1cc134112e4314c10\"]/button"));
+        submitButton.click();
+
+
+
+    }
+
+}
