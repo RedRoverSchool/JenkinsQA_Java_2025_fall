4faf72547aeec8115547f65f3aa86dbb160348b8
KobusTA25 <125394170+KobusTA25@users.noreply.github.com>
2025-10-05 18:37:28 +0300

    Assert add

--- src/test/java/school/redrover/TatianaKTest.java
+++ src/test/java/school/redrover/TatianaKTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TatianaKTest {
@@ -17,11 +18,11 @@ public class TatianaKTest {
         WebElement simpleModalButton = driver.findElement(By.xpath("//*[@id=\"formModal\"]"));
         simpleModalButton.click();
 
-        WebElement textName = driver.findElement(By.xpath("//*[@id=\"g1051-name\"]"));
+        WebElement textName = driver.findElement(By.cssSelector("input#g1051-name"));
         textName.sendKeys("Tatiana");
 
-        WebElement textEmail = driver.findElement(By.xpath("//*[@id=\"g1051-email\"]"));
-        textEmail.sendKeys("Tatiana@gmail.com");
+        WebElement textEmail = driver.findElement(By.cssSelector("input#g1051-email"));
+        textEmail.sendKeys("Ttt");
 
         WebElement textMessage = driver.findElement(By.xpath("//*[@id=\"contact-form-comment-g1051-message\"]"));
         textMessage.sendKeys("Hi! How are you?");
@@ -29,7 +30,8 @@ public class TatianaKTest {
         WebElement submitButton = driver.findElement(By.xpath("//*[@id=\"jp-form-99ddf6d0cf76daf0c77607b1cc134112e4314c10\"]/button"));
         submitButton.click();
 
-
+        WebElement message = driver.findElement(By.xpath("//*[@id=\"jp-form-99ddf6d0cf76daf0c77607b1cc134112e4314c10\"]/div[4]/ul/li/a"));
+        Assert.assertEquals(message.getText(), "Email : Please enter a valid email address");
 
     }
 
