841c9b8f76e535d806082608c503d02e43bd8f79
Stanislav Agafonov <chaltstas@yandex.ru>
2025-10-05 18:41:35 +0300

    add class StasATest and testPutDepositBankAccount. add import class SergeiButorinTest

--- src/test/java/school/redrover/SergeiButorinTest.java
+++ src/test/java/school/redrover/SergeiButorinTest.java
@@ -2,15 +2,16 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class SergeiButorinTest{
+import java.time.Duration;
+
+public class SergeiButorinTest {
 
     @Test
-    public static void testHeadhunter(){
+    public static void testHeadhunter() {
         WebDriver hhdriver = new ChromeDriver();
         hhdriver.get("https://www.hh.ru/vacancy/122718353");
         hhdriver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
