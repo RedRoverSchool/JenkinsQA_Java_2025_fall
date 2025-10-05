0eb9cab981bb663daffdceeafda5276f1579b1f3
JuliaShemy <Juliashem@gmail.com>
2025-10-05 17:54:53 +0400

    Add first test

--- src/test/java/school/redrover/JuliaShemTest.java
+++ src/test/java/school/redrover/JuliaShemTest.java
@@ -4,12 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
-
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
