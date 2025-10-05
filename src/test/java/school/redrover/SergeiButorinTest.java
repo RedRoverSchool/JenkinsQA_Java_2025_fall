00d45ffb4f22731efb412a11908f5dd5a268b681
Сергей <aikon@bk.ru>
2025-10-05 20:59:27 +0500

    sb/Create own test(correction3)

--- src/test/java/school/redrover/SergeiButorinTest.java
+++ src/test/java/school/redrover/SergeiButorinTest.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class SergeiButorinTest{
 
     @Test
