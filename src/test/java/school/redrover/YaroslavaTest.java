94287658a53c1bdb63b5eb399d7b2fe17615634a
Yaroslavasg <garmash01@gmail.com>
2025-10-07 07:51:12 -0400

    Add the annotation After Method.

--- src/test/java/school/redrover/YaroslavaTest.java
+++ src/test/java/school/redrover/YaroslavaTest.java
@@ -4,22 +4,59 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class YaroslavaTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void startBeforeTest() {
+        driver = new ChromeDriver();
+        driver.get("https://automationexercise.com/");
+    }
+
     @Test
     public void testProductsButton() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://automationexercise.com/");
 
         WebElement products = driver.findElement(By.xpath("//a[@href='/products']"));
-        System.out.println(products.getText());
-
 
         Assert.assertEquals(products.getText(), " Products");
-
-        driver.quit();
     }
 
+    @Test
+    public void testEmail() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://automationexercise.com/");
+
+        WebElement cart = driver.findElement(By.xpath("//a[@href='/view_cart']"));
+        cart.click();
+
+        WebElement field = driver.findElement(By.id("susbscribe_email"));
+        field.sendKeys("Ted@gmail.com");
+
+        WebElement go = driver.findElement(By.cssSelector(".fa.fa-arrow-circle-o-right"));
+        go.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement successfulMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector(".alert-success.alert")));
+
+        Assert.assertEquals(successfulMessage.getText(), "You have been successfully subscribed!");
+    }
+
+    @AfterMethod
+    public void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
