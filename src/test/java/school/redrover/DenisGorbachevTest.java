04dab89f62b46f1f1e2540b692d4614931bd32e0
Garrett <8141816@gmail.com>
2025-10-08 00:44:33 +0300

    updated tests as Hw15 in accordance with reviews

--- src/test/java/school/redrover/DenisGorbachevTest.java
+++ src/test/java/school/redrover/DenisGorbachevTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -16,12 +17,12 @@ public class DenisGorbachevTest {
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
         driver.getTitle();
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-        WebElement textBox = driver.findElement(By.xpath("/html/body/main/div/form/div/div[1]/label[3]/textarea"));
-        WebElement submitButton = driver.findElement(By.xpath("/html/body/main/div/form/div/div[2]/button"));
+        WebElement textBox = driver.findElement(By.xpath("//textarea[@name='my-textarea']"));
+        WebElement submitButton = driver.findElement(By.xpath("//button"));
         textBox.sendKeys("Selenium Test");
         submitButton.click();
         WebElement message = driver.findElement(By.id("message"));
-        message.getText();
+        Assert.assertEquals(message.getText(), "Received!");
         driver.quit();
     }
 
@@ -32,21 +33,15 @@ public class DenisGorbachevTest {
         driver.get("https://habr.com/");
         driver.getTitle();
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-        WebElement searchButton = driver.findElement(By.cssSelector("#app > div > header > div > div > div > a.tm-header-user-menu__item.tm-header-user-menu__search > svg"));
+        WebElement searchButton = driver.findElement(By.cssSelector("[class*='tm-header-user-menu__search']"));
         searchButton.click();
-        WebElement textBox = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/main/div/div/div/div[1]/div/div[2]/form/div/input"));
-        WebElement submitButton = driver.findElement(By.cssSelector("#app > div > div.tm-layout > main > div > div > div > div.tm-page__main.tm-page__main_has-sidebar > div > div.tm-page__top > form > div > div > span > svg"));
+        WebElement textBox = driver.findElement(By.cssSelector("[class*='tm-search__input tm-input-text-decorated__input']"));
+        WebElement submitButton = driver.findElement(By.cssSelector("[class*='tm-search__icon']"));
+
         textBox.sendKeys("Selenium Test");
         submitButton.click();
         WebElement message = driver.findElement(By.className("searched-item"));
-        if (message.getText().contains("Selenium Test")){
+        Assert.assertEquals(message.getText(), "Selenium Tests");
             driver.quit();
-        }
-        else {
-            System.out.println(message.getText());
-            System.out.println("Failed");
-            driver.quit();
-        }
-
     }
 }
