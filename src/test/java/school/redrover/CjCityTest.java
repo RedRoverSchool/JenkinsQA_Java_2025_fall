f9e7c7d0b210e0a8b98dc48e6b2105f2f9c34d5f
Igor <osinus-sb@ya.ru>
2025-10-08 02:04:53 +0700

    1) replaced junit with testing 2) deleted my changes in POM

--- src/test/java/school/redrover/CjCityTest.java
+++ src/test/java/school/redrover/CjCityTest.java
@@ -1,6 +1,6 @@
 package school.redrover;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
