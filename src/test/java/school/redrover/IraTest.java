52df54f24e16d7a59ce19faeabf0a5694fa8c9e6
ISokolova <issklva@gmail.com>
2025-10-07 10:30:02 +0500

    Add  logoutTest

--- src/test/java/school/redrover/IraTest.java
+++ src/test/java/school/redrover/IraTest.java
@@ -1,9 +1,12 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -32,7 +35,7 @@ public class IraTest {
     public void addToCardTest(){
         WebDriver driver = new ChromeDriver();
             driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
             driver.get("https://www.saucedemo.com/");
             driver.findElement(By.id("user-name")).sendKeys("standard_user");
             driver.findElement(By.id("password")).sendKeys("secret_sauce");
@@ -43,6 +46,25 @@ public class IraTest {
         driver.findElement(By.id("add-to-cart")).click();
         String basket = driver.findElement(By.cssSelector(".shopping_cart_badge")).getText();
         Assert.assertEquals(basket,"1");
+        }
+
+
+        @Test
+    public void logoutTest()  {
+            WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+            driver.get("https://www.saucedemo.com/");
+            driver.findElement(By.id("user-name")).sendKeys("standard_user");
+            driver.findElement(By.id("password")).sendKeys("secret_sauce");
+            driver.findElement(By.xpath("//input[@class='submit-button btn_action']")).click();
+            WebElement header = driver.findElement(By.xpath("//div[@class='header_label']"));
+            Assert.assertEquals(header.getText(), "Swag Labs");
+            driver.findElement(By.id("react-burger-menu-btn")).click();
+            driver.findElement(By.id("logout_sidebar_link")).click();
+                driver.findElement(By.id("logout_sidebar_link")).click();
+            Assert.assertTrue(driver.findElement(By.id("user-name")).getAttribute("value").isEmpty());
+            Assert.assertTrue(driver.findElement(By.id("user-name")).getAttribute("placeholder").equals("Username"));
 
         }
 }
