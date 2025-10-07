b3c847492fd00ab1cc63d9cf63b5665a4403883b
Yuliya Kuzz <jkuzmina84@gmail.com>
2025-10-07 20:56:57 +0400

    Add my first test

---
+++ src/test/java/school/redrover/YuliyaTest.java
@@ -0,0 +1,26 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class YuliyaTest {
+            @Test
+        public void testEnter() throws InterruptedException {
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://dev.trexer.ru/auth/login");
+            System.out.println(driver.getTitle());
+            //driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+            Thread.sleep(500);
+            WebElement emailLabel = driver.findElement(By.cssSelector("input"));
+            emailLabel.sendKeys("prodigy@mail.ru");
+
+            WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+            Assert.assertTrue(submitButton.isDisplayed(), "Элемент не активен");
+            driver.quit();
+        }
+}
