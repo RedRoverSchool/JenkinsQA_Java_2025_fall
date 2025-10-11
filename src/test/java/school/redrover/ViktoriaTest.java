8b8cb6c24969bef68465b459785618a2c6207859
Roman Burlaka <burlakarv@gmail.com>
2025-10-11 17:25:40 +0200

    added testGetUrl for VictoriaTest

--- src/test/java/school/redrover/ViktoriaTest.java
+++ src/test/java/school/redrover/ViktoriaTest.java
@@ -4,16 +4,42 @@ import com.sun.source.tree.AssertTree;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class ViktoriaTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://automationexercise.com/");
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        if (result.isSuccess()) {
+            driver.quit();
+            System.out.println("Test passed :" + result.getName());
+        }
+        else {
+            System.out.println("Test failed :" + result.getName());
+        }
+
+    }
 
     @Test
     public void testGetTile() {
-        WebDriver driver = new ChromeDriver();
-
-        driver.get("https://automationexercise.com/");
         Assert.assertEquals(driver.getTitle(),"Automation Exercise");
-        driver.quit();
+
     }
+
+    @Test
+    public void testGetUrl() {
+        Assert.assertEquals(driver.getCurrentUrl() ,"https://automationexercise.com/");
+
+    }
+
 }
