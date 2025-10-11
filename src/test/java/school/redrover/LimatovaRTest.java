89e0559486d2a2b99a09baedb90c6204691bb729
Рукият <rukiyat@MacBook-Air-Rukiat.local>
2025-10-11 23:36:39 +0300

    Added one positive and one negative test

---
+++ src/test/java/school/redrover/LimatovaRTest.java
@@ -0,0 +1,62 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static java.time.Duration.ofMillis;
+import static org.openqa.selenium.By.name;
+
+public class LimatovaRTest {
+
+    WebDriver driver;
+    public static final String LOGIN = "standard_user";
+    public static final String PASSWORD = "secret_sauce";
+
+    @BeforeTest
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(ofMillis(3000));
+    }
+
+    @Test
+    public void LoginTest() {
+
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(name("user-name")).sendKeys(LOGIN);
+        driver.findElement(name("password")).sendKeys(PASSWORD);
+        driver.findElement(name("login-button")).click();
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html");
+        Assert.assertEquals(driver.getTitle(), "Swag Labs");
+    }
+
+    @Test
+    public void NegativeTest() {
+
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.xpath("//input[@id='user-name']"))
+                .sendKeys("123");
+        driver.findElement(By.xpath("//input[@id='password']"))
+                .sendKeys("123");
+        driver.findElement(By.xpath("//input[@id='login-button']")).click();
+
+        WebElement errorNotification = driver.findElement(By.xpath("//h3[text()='Epic sadface: " +
+                "Username and password do not match any user in this service']"));
+
+        Assert.assertNotNull(errorNotification);
+    }
+
+    @AfterTest
+    private void close() {
+        driver.quit();
+    }
+}
