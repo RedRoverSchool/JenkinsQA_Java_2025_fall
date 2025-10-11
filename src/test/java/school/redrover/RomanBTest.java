3d9382c72da1f50d921f91bbf1220f08c1018dda
Roman Burlaka <burlakarv@gmail.com>
2025-10-11 16:23:32 +0200

    testClassAttribute for RomanBtest

--- src/test/java/school/redrover/RomanBTest.java
+++ src/test/java/school/redrover/RomanBTest.java
@@ -16,7 +16,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-
 import java.time.Duration;
 
 
