b3ea2cfdd710a96944317b9518f909623ea208a2
Anna Sadovskaya <anna.sadovskaya.v@gmail.com>
2025-10-12 15:29:01 -0700

    Added new test

--- src/test/java/school/redrover/AnnaSadovskayaTest.java
+++ src/test/java/school/redrover/AnnaSadovskayaTest.java
@@ -135,14 +135,15 @@ public class AnnaSadovskayaTest {
     public void searchProduct() {
         /*
          Launch browser
-2. Navigate to url 'http://automationexercise.com'
-3. Verify that home page is visible successfully
-4. Click on 'Products' button
-5. Verify user is navigated to ALL PRODUCTS page successfully
-6. Enter product name in search input and click search button
-7. Verify 'SEARCHED PRODUCTS' is visible
-8. Verify all the products related to search are visible
+        2. Navigate to url 'http://automationexercise.com'
+        3. Verify that home page is visible successfully
+        4. Click on 'Products' button
+        5. Verify user is navigated to ALL PRODUCTS page successfully
+        6. Enter product name in search input and click search button
+        7. Verify 'SEARCHED PRODUCTS' is visible
+        8. Verify all the products related to search are visible
          */
+
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://automationexercise.com/");
