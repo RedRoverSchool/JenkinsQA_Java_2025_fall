91775a86ff26c3ab373cde31ae36fd912c789f98
SergeyVyshkvarok <sergey.vyshkvarok@gmail.com>
2025-10-13 23:59:58 +0900

    first test RF

--- src/test/java/school/redrover/SergeyVyshkvarokTest.java
+++ src/test/java/school/redrover/SergeyVyshkvarokTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
