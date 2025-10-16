1465ffed9b234ae8af20c0285b1f776aafc186c4
Dinara Gaynanova <gaynanovadr@me.com>
2025-10-15 19:35:49 -0500

    @Ignore added for one test

--- src/test/java/school/redrover/BitByBitGroupTest.java
+++ src/test/java/school/redrover/BitByBitGroupTest.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -233,7 +234,9 @@ public class BitByBitGroupTest {
         driver.quit();
       }
 
+
     @Test
+    @Ignore
     public void testPriceOfBooking()  {
         final int numDays = 3;
         final int expectedTotal = 100 * (numDays + 1) + 25 + 15;
