e5de686c4890eb41ce9df57c33c5f4303c5f7f53
Jevgenijs <judzhin426@gmail.com>
2025-10-16 16:14:10 +0300

    JevTest (#187)
    * testAutomationExercise added
    
    * Class JevTest deleted .

--- src/test/java/school/redrover/GroupTheBugStopsHereTest.java
+++ src/test/java/school/redrover/GroupTheBugStopsHereTest.java
@@ -45,4 +45,19 @@ public class GroupTheBugStopsHereTest {
 
         driver.quit();
     }
+    @Test
+    public void testAutomationExercise() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://automationexercise.com/test_cases");
+        driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/div[2]/div[2]/button[1]/p")).click();
+        driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a")).click();
+        driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/input[2]")).sendKeys("ememdems@hotmail.com");
+        driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/input[3]")).sendKeys("8XbTY@zG@wYg2hg");
+        driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/button")).click();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a")).isDisplayed());
+
+        driver.quit();
+    }
 }
