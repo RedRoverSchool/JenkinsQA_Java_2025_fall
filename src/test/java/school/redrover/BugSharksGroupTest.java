1a1c5150f0c4705c49c2a7fbde0e35fc97ed3670
StepanidaKirillina1 <steshakirillina@yandex.ru>
2025-10-16 15:03:23 +0900

    move tests to the group's class remove my individual class

--- src/test/java/school/redrover/BugSharksGroupTest.java
+++ src/test/java/school/redrover/BugSharksGroupTest.java
@@ -2,18 +2,35 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class BugSharksGroupTest {
+    private WebDriver driver;
+    private static final String PASSWORD = "secret_sauce";
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 
     @Test
     public void testSuccessfulLoginPos() {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
         driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
@@ -22,14 +39,10 @@ public class BugSharksGroupTest {
 
         Assert.assertEquals(driver.findElement(By.xpath("//h6[text()='Dashboard']")).getText(),
                 "Dashboard");
-
-        driver.quit();
     }
 
     @Test
     public void testWrongCredentialsLoginNeg() {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
         driver.findElement(By.xpath("//input[@name='username']")).sendKeys("wrongLogin");
@@ -38,14 +51,10 @@ public class BugSharksGroupTest {
 
         Assert.assertEquals(driver.findElement(By.xpath("//p[text()='Invalid credentials']"))
                 .getText(), "Invalid credentials");
-
-        driver.quit();
     }
 
     @Test
     public void testEmptyCredentialsLoginNeg() {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
         driver.findElement(By.xpath("//button[@type='submit']")).click();
@@ -56,7 +65,39 @@ public class BugSharksGroupTest {
         Assert.assertEquals(driver.findElement(By
                         .xpath("//label[text()='Password']/ancestor::div/span[text()='Required']")).getText(),
                 "Required");
+    }
 
-        driver.quit();
+    @Test
+    public void testSauceDemoHomePageTitle() {
+        openSauceDemoHomePage();
+
+        String actualTitle = driver.getTitle();
+
+        Assert.assertEquals(actualTitle, "Swag Labs");
+    }
+
+    @DataProvider(name = "userCredentials")
+    public Object[][] provideUserCredentials() {
+        return new Object[][]{
+                {"standard_user", PASSWORD},
+                {"performance_glitch_user", PASSWORD}
+        };
+    }
+
+    @Test(dataProvider = "userCredentials")
+    public void testSauceDemoSuccessfulLogin(String username, String password) {
+        openSauceDemoHomePage();
+
+        driver.findElement(By.id("user-name")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("login-button")).click();
+
+        WebElement appLogo = driver.findElement(By.className("app_logo"));
+
+        Assert.assertEquals(appLogo.getText(), "Swag Labs");
+    }
+
+    private void openSauceDemoHomePage() {
+        driver.get("https://www.saucedemo.com/");
     }
 }
