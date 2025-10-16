f363101d1d4ff47d79e5bcf3eb02450bbbcef5a8
Deviona <deviona0190@gmail.com>
2025-10-16 21:55:35 +0900

    RF rename

--- src/test/java/school/redrover/GroupTheBugStopsHereTest.java
+++ src/test/java/school/redrover/GroupTheBugStopsHereTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class GroupTheBugStopsHere {
+public class GroupTheBugStopsHereTest {
 
     @Test
     public void testAddReview() {
