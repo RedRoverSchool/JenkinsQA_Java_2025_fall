7da2122d2c03ee9c7e7cdc78ae9b7ab74bdff4f6
Evgeny Chernyavskiy <evgg90@gmail.com>
2025-10-16 22:26:08 +0700

    My test drom.ru was moved into GroupCodeCoffeJavaTest

--- src/test/java/school/redrover/GroupCodeCoffeeJavaTest.java
+++ src/test/java/school/redrover/GroupCodeCoffeeJavaTest.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class GroupCodeCoffeeJavaTest {
     @Test
     public void testDemoQa() throws InterruptedException {
@@ -34,5 +36,44 @@ public class GroupCodeCoffeeJavaTest {
 
         Thread.sleep(3000);
         driver.close();
+    }
+
+    @Test
+    public void testNegativeAuthorization() {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://drom.ru/");
+
+        driver.findElement(By.cssSelector("body > div:nth-child(7) > div._1e74g3sa.css-10ib5jr > div > a")).click();
+        driver.findElement(By.id("signbutton")).click();
+
+        driver.findElement(By.id("sign")).sendKeys("admin");
+        driver.findElement(By.id("password")).sendKeys("admin");
+        driver.findElement(By.id("signbutton")).click();
+
+        WebElement error = driver.findElement(By.id("sign_errors"));
+        Assert.assertEquals(error.getText(), "Данные для входа неверны");
+
+        driver.quit();
+    }
+    @Test
+    public void testSearchToyota() {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://drom.ru/");
+
+        driver.findElement(By.cssSelector("body > div:nth-child(7) > div._1e74g3sa.css-10ib5jr > div > div._1e74g3sd > a:nth-child(1)")).click();
+        driver.findElement(By.xpath("/html/body/div[2]/div[5]/div[1]/div[1]/div[3]/form/div/div[1]/div[1]/div/div[1]/input")).click();
+        driver.findElement(By.xpath("/html/body/div[2]/div[5]/div[1]/div[1]/div[3]/form/div/div[1]/div[1]/div/div[2]/div/div[3]/div")).click();
+        driver.findElement(By.xpath("/html/body/div[2]/div[5]/div[1]/div[1]/div[3]/form/div/div[4]/div[3]/button/div")).click();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
+
+        String toyotaAd = driver.findElement(By.xpath("/html/body/div[2]/div[5]/div[1]/div[1]/div[5]/div/div[1]/div[1]/div[2]/div[1]/a/h3")).getText();
+        Assert.assertEquals(toyotaAd.substring(0, 6), "Toyota");
+
+        driver.quit();
     }
 }
