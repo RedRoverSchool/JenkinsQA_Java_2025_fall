3a29ff407be3b2c2993fd33017248ace42f1c548
AnnKolesnik <annbezd@gmail.com>
2025-10-17 17:49:55 +0300

    akol merge in groupUnited

--- src/test/java/school/redrover/GroupUnitedByJavaTest.java
+++ src/test/java/school/redrover/GroupUnitedByJavaTest.java
@@ -977,5 +977,30 @@ public class GroupUnitedByJavaTest {
 
         closeDriver();
     }
+
+    @Test
+    public void testDataOpeningL2() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.la2era.com/ru");
+        WebElement textBox = driver.findElement(By.xpath("/html/body/div/section/section/div/div/div[2]/div[2]/a[1]"));
+        textBox.click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("body > div > section > section > div > div > div.home-about__stroke.flex-cc > div.tab-content > div.tab-pane.active > div.home-about__stroke.flex-ss > div:nth-child(9)")).getText(), "Дата открытия: 09.02.2018");
+
+        driver.quit();
+    }
+
+    @Test
+    public void testChangeLanguageL2() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.la2era.com/ru");
+        WebElement submitButton = driver.findElement(By.cssSelector("body > div.wrapper > header > nav > div > div.navigation__langs > div"));
+        submitButton.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
+        WebElement submitButton2 = driver.findElement(By.cssSelector("body > div > header > nav > div > div.navigation__langs > div > a:nth-child(3) > div.navigation__langs-item-name"));
+        submitButton2.click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("body > div.wrapper > section > section > div > div > div.home-about__textbox > div")).getText(), "HIGH FIVE X3: LAUNCHING OCTOBER 3RD.\n" + "GET READY FOR ADVENTURE!");
+
+        driver.quit();
+    }
 }
 
