72c4c3c3730ad26d7e28cc023c95729ec8fc125c
Maya Makeeva <mvmakeeva@mail.ru>
2025-10-17 13:31:22 +0700

    correct locators

--- src/test/java/school/redrover/MayaTest.java
+++ src/test/java/school/redrover/MayaTest.java
@@ -16,11 +16,10 @@ public class MayaTest {
         driver.get("https://www.demoblaze.com/");
         driver.manage().window().maximize();
 
-        driver.findElement(By.xpath("//*[@id=\"login2\"]")).click();
-
+        driver.findElement(By.id("login2")).click();
         driver.findElement(By.xpath("//*[@id='loginusername']")).sendKeys("mTest@gmail.com");
         driver.findElement(By.xpath("//*[@id='loginpassword']")).sendKeys("45784okng_75()");
-        driver.findElement(By.xpath("//*[@id='logInModal']/div/div/div[3]/button[2]")).click();
+        driver.findElement(By.xpath("//button[text()='Log in']")).click();
 
         Assert.assertTrue(driver.findElement(By.xpath("//*[@id='logout2']")).isDisplayed(),
                 "Success");
