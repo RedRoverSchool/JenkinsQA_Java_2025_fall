2392432e44f5d1733b99faf3b7d891c07cebafbe
Yaroslavasg <garmash01@gmail.com>
2025-10-17 09:59:45 -0400

    Migrate test from personal class to group class

--- src/test/java/school/redrover/BugSharksGroupTest.java
+++ src/test/java/school/redrover/BugSharksGroupTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -13,16 +14,25 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BugSharksGroupTest {
 
     private WebDriver driver;
     private WebDriverWait wait;
     private static final String PASSWORD = "secret_sauce";
+    private static final String BASE_URL_SD = "https://www.saucedemo.com";
+    private static final String PRODUCT_PAGE_URL_SD = BASE_URL_SD + "/inventory.html";
+    private static final String USERNAME = "yar"+ System.currentTimeMillis();
+    private static final String USEREMAIL = USERNAME + "@gmail.com";
 
     @BeforeMethod
     public void setUp() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
@@ -34,7 +44,11 @@ public class BugSharksGroupTest {
         }
     }
 
-    private void automationexercise(){
+    private void openSauceDemoHomePage() {
+        driver.get(BASE_URL_SD);
+    }
+
+    private void automationexercise() {
         driver.get("https://automationexercise.com/");
     }
 
@@ -106,17 +120,14 @@ public class BugSharksGroupTest {
         Assert.assertEquals(appLogo.getText(), "Swag Labs");
     }
 
-    private void openSauceDemoHomePage() {
-        driver.get("https://www.saucedemo.com/");
-    }
-
     @Test
-    public void testProductsButton () {
+    public void testProductsButton() {
         automationexercise();
         WebElement products = driver.findElement(By.xpath("//a[@href='/products']"));
 
         Assert.assertEquals(products.getText(), " Products");
     }
+
     @Test
     public void testEmail() {
         automationexercise();
@@ -134,4 +145,122 @@ public class BugSharksGroupTest {
 
         Assert.assertEquals(successfulMessage.getText(), "You have been successfully subscribed!");
     }
+
+    @Test
+    public void testLogIn() throws InterruptedException {
+        automationexercise();
+        Thread.sleep(100);
+        WebElement signUplogInButton = driver.findElement(By.xpath("//a[@href='/login']"));
+        signUplogInButton.click();
+
+        WebElement loginField = wait.until(ExpectedConditions.visibilityOfElementLocated
+                (By.xpath("//form//input[@data-qa='signup-name']")));
+        loginField.sendKeys(USERNAME);
+
+        WebElement userPassword = driver.findElement(By.xpath("//form//input[@data-qa='signup-email']"));
+        userPassword.sendKeys(USEREMAIL);
+
+        WebElement signUpButton = driver.findElement(By.xpath("//form//button[@data-qa='signup-button']"));
+        signUpButton.click();
+
+        WebElement createButton = wait.until(ExpectedConditions.visibilityOfElementLocated
+                (By.xpath("//form//button[@data-qa='create-account']")));
+
+        Assert.assertEquals(createButton.getText(),"Create Account");
+    }
+
+    public void loginStandartUser() {
+        openSauceDemoHomePage();
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys(PASSWORD);
+        driver.findElement(By.id("login-button")).click();
+    }
+
+    @Test
+    public void testCorrectProductsPage() {
+        loginStandartUser();
+
+        WebElement titleProductPage = driver.findElement(By.xpath(".//div[@id=\"header_container\"]/div[2]/span"));
+
+        Assert.assertEquals(driver.getTitle(), "Swag Labs");
+        Assert.assertEquals(driver.getCurrentUrl(), PRODUCT_PAGE_URL_SD);
+        Assert.assertEquals(titleProductPage.getText(), "Products");
+    }
+
+    @Test
+    public void testBackToProductsButton() {
+        loginStandartUser();
+
+        WebElement productName = driver.findElement(By.xpath(".//*[@id=\"item_2_title_link\"]/div"));
+        productName.click();
+
+        WebElement backToProductsButton = driver.findElement(By.id("back-to-products"));
+        Assert.assertEquals(backToProductsButton.getText(), "Back to products");
+
+        backToProductsButton.click();
+
+        Assert.assertEquals(driver.getCurrentUrl(), PRODUCT_PAGE_URL_SD);
+    }
+
+    @Test
+    public void testCorrectAllProductName() {
+        loginStandartUser();
+
+        List<WebElement> productNamesList = driver.findElements(By.xpath(".//div[@class=\"inventory_item_name \"]"));
+
+        List<String> namesList = productNamesList.stream()
+                .map(product -> product.getText())
+                .toList();
+
+        List<String> currentNamesList = new ArrayList<>();
+
+        for (int i = 0; i < productNamesList.size(); i++) {
+            productNamesList = driver.findElements(By.xpath(".//div[@class=\"inventory_item_name \"]"));
+            productNamesList.get(i).click();
+
+            WebElement currentName = driver.findElement(By.xpath(".//div[@class=\"inventory_details_desc_container\"]/div"));
+            currentNamesList.add(currentName.getText());
+
+            driver.findElement(By.id("back-to-products")).click();
+        }
+        Assert.assertEquals(namesList, currentNamesList);
+    }
+
+    @Test
+    public void testCorrectProductPrice() {
+        loginStandartUser();
+
+        String priceByProductPage = driver.findElement(By
+                        .xpath(".//*[@id=\"item_2_title_link\"]/ancestor::div[@class=\"inventory_item_description\"]//div[@class=\"pricebar\"]/div"))
+                .getText();
+
+        WebElement productName = driver.findElement(By.xpath(".//*[@id=\"item_2_title_link\"]/div"));
+        productName.click();
+
+        String priceByItemPage = driver.findElement(By
+                        .xpath(".//div[@class=\"inventory_details_price\"]"))
+                .getText();
+
+        Assert.assertEquals(priceByItemPage, priceByProductPage);
+    }
+
+    @Test
+    public void testCorrectProductImageTest() {
+        loginStandartUser();
+
+        String imageByProductPage = driver.findElement(By
+                        .xpath(".//*[@id=\"item_2_img_link\"]/img"))
+                .getAttribute("src");
+
+        WebElement productName = driver.findElement(By.xpath(".//*[@id=\"item_2_title_link\"]/div"));
+        productName.click();
+
+        String imageByItemPage = driver.findElement(By
+                        .xpath(".//div[@class=\"inventory_details_img_container\"]/img"))
+                .getAttribute("src");
+
+        Assert.assertEquals(imageByItemPage, imageByProductPage);
+    }
+
 }
