c1ca7f4c687db8d8bbb8f174f1c20631eeee58a5
kirillbokhenek <kirill.bohenek@gmail.com>
2025-10-17 15:31:39 +0300

    Add testcase 2-5

--- src/test/java/school/redrover/Breadcrumbs.java
+++ src/test/java/school/redrover/Breadcrumbs.java
@@ -66,4 +66,114 @@ public class Breadcrumbs {
 
         driver.quit();
     }
+
+    @Test
+    public void testautomationexercise_case2() {
+        String namelogin = "newuser";
+        String email = "newuser@supermail.com";
+        String password = "dvdsgfsdfsd987654321";
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://automationexercise.com/");
+        Assert.assertEquals(driver.getTitle(), "Automation Exercise");
+        WebElement homeButton = driver.findElement(By.cssSelector(".shop-menu li > a[href=\"/\"]"));
+        String color = homeButton.getAttribute("style");  // color: orange;
+        System.out.println(homeButton.getAttribute("style"));
+        Assert.assertTrue(color.contains("color: orange;"), "Кнопка не оранжевая");
+        driver.findElement(By.cssSelector(".shop-menu li > a[href=\"/login\"")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector(".signup-form h2")).getText(), "New User Signup!");
+        driver.findElement(By.cssSelector(".shop-menu li > a[href=\"/login\"")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector(".signup-form h2")).getText(), "New User Signup!");
+        WebElement loginname = driver.findElement(By.cssSelector("[data-qa='signup-name']"));
+        loginname.sendKeys(namelogin);
+        driver.findElement(By.cssSelector("[data-qa='signup-email']")).sendKeys(email);
+        driver.findElement(By.cssSelector("[data-qa='signup-button']")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.login-form > h2 > b")).getText().toLowerCase(), "enter account information");
+        driver.findElement(By.id("id_gender1")).click();
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.cssSelector("[data-qa='days'] :nth-child(19)")).click();
+        driver.findElement(By.cssSelector("[data-qa='months'] :nth-child(8)")).click();
+        driver.findElement(By.cssSelector("[data-qa='years'] :nth-child(40)")).click();
+        driver.findElement(By.id("newsletter")).click();
+        driver.findElement((By.id("first_name"))).sendKeys("User");
+        driver.findElement((By.id("last_name"))).sendKeys("User");
+        driver.findElement((By.id("company"))).sendKeys("Company");
+        driver.findElement((By.id("address1"))).sendKeys("Street, 33, 12345, Unknown");
+        driver.findElement((By.id("address2"))).sendKeys("Unknown");
+        driver.findElement(By.cssSelector("[data-qa='country'] :nth-child(3)")).click();
+        driver.findElement((By.id("state"))).sendKeys("Unknown");
+        driver.findElement((By.id("city"))).sendKeys("Unknown");
+        driver.findElement((By.id("zipcode"))).sendKeys("12345");
+        driver.findElement((By.id("mobile_number"))).sendKeys("+12322245");
+        driver.findElement(By.cssSelector("[data-qa='create-account']")).click();
+        driver.findElement(By.cssSelector("[data-qa='continue-button']")).click();
+        driver.findElement(By.cssSelector(".shop-menu  > ul > :nth-child(4)")).click();
+        driver.findElement(By.cssSelector(".shop-menu li > a[href=\"/login\"")).click();
+        driver.findElement(By.cssSelector("[data-qa='login-email']")).sendKeys(email);
+        driver.findElement(By.cssSelector("[data-qa='login-password']")).sendKeys(password);
+        driver.findElement(By.cssSelector("[data-qa='login-button']")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector(".shop-menu > ul > :nth-child(10) > a > b")).getText(), namelogin);
+        driver.findElement(By.cssSelector(".shop-menu > ul > :nth-child(5)")).click();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("[data-qa='account-deleted'] > b")).getText(), "ACCOUNT DELETED!");
+        driver.findElement(By.cssSelector("[data-qa='continue-button']")).click();
+
+        driver.quit();
+    }
+
+    @Test
+    public void testautomationexercise_case3() {
+        String namelogin = "newuser";
+        String email = "newuser@supermail.com";
+        String password = "dvdsgfsdfsd987654dgdfgdfgfsgd321";
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://automationexercise.com/");
+        driver.findElement(By.cssSelector(".shop-menu li > a[href=\"/login\"")).click();
+        driver.findElement(By.cssSelector("[data-qa='login-email']")).sendKeys(email);
+        driver.findElement(By.cssSelector("[data-qa='login-password']")).sendKeys(password);
+        driver.findElement(By.cssSelector("[data-qa='login-button']")).click();
+
+        Assert.assertTrue(driver.findElement(By.cssSelector(".login-form > form > :nth-child(4)")).isDisplayed(), "Your email or password is incorrect!");
+        driver.quit();
+    }
+
+    @Test
+    public void testautomationexercise_4() {
+        String namelogin = "gygygy";
+        String email = "gygygy@supermail.com";
+        String password = "dvdsgfsdfsd987654321";
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://automationexercise.com/");
+        driver.findElement(By.cssSelector(".shop-menu li > a[href=\"/login\"")).click();
+        driver.findElement(By.cssSelector("[data-qa='login-email']")).sendKeys(email);
+        driver.findElement(By.cssSelector("[data-qa='login-password']")).sendKeys(password);
+        driver.findElement(By.cssSelector("[data-qa='login-button']")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector(".shop-menu > ul > :nth-child(10) > a > b")).getText(), namelogin);
+        driver.findElement(By.cssSelector(".shop-menu  > ul > :nth-child(4)")).click();
+        driver.quit();
+    }
+
+    @Test
+    public void testautomationexercise_5() {
+        String namelogin = "gygygy";
+        String email = "gygygy@supermail.com";
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://automationexercise.com/");
+        Assert.assertEquals(driver.getTitle(), "Automation Exercise");
+        WebElement homeButton = driver.findElement(By.cssSelector(".shop-menu li > a[href=\"/\"]"));
+        String color = homeButton.getAttribute("style");  // color: orange;
+        System.out.println(homeButton.getAttribute("style"));
+        Assert.assertTrue(color.contains("color: orange;"), "Кнопка не оранжевая");
+
+        driver.findElement(By.cssSelector(".shop-menu li > a[href=\"/login\"")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector(".signup-form h2")).getText(), "New User Signup!");
+
+        driver.findElement(By.cssSelector("[data-qa='signup-name']")).sendKeys(namelogin);
+        driver.findElement(By.cssSelector("[data-qa='signup-email']")).sendKeys(email);
+        driver.findElement(By.cssSelector("[data-qa='signup-button']")).click();
+
+        System.out.println(driver.findElement(By.cssSelector(".signup-form > form > :nth-child(5)")).getText());
+        Assert.assertEquals(driver.findElement(By.cssSelector(".signup-form > form > :nth-child(5)")).getText(), "Email Address already exist!");
+        driver.quit();
+    }
 }
