39e9964cd6a0848da6d66acbfc43a4dfd078f603
Kate.M <ma_ka_r0va@bk.ru>
2025-10-16 20:29:19 -0500

    Add tests from KMakaravaTest.java to GroupJavaCrackersRedroverTest.java

--- src/test/java/school/redrover/GroupJavaCrackersRedroverTest.java
+++ src/test/java/school/redrover/GroupJavaCrackersRedroverTest.java
@@ -8,6 +8,9 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+import java.util.List;
+
 public class GroupJavaCrackersRedroverTest {
 
     public static void scrollAndClick(Actions actions, WebElement element) {
@@ -69,6 +72,67 @@ public class GroupJavaCrackersRedroverTest {
 
         WebElement message = driver.findElement(By.id("output"));
         Assert.assertTrue(message.isDisplayed(), "Please verify reCaptcha to register!");
+
+        driver.quit();
+    }
+
+    @Test
+    public void testSelenium(){
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        Assert.assertEquals(message.getText(), "Received!");
+        driver.quit();
+    }
+
+    @Test
+    public void testSwagLabs() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to("https://www.saucedemo.com/");
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+        driver.findElement(By.name("login-button")).click();
+        WebElement secondTitle = driver.findElement(By.cssSelector(".title"));
+        Assert.assertEquals(driver.getTitle(), "Swag Labs");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html");
+        Assert.assertEquals(secondTitle.getText(), "Products");
+
+        driver.quit();
+    }
+
+    @Test
+    public void testHerokuappOpeningAddRemoveElementsPage(){
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to("https://the-internet.herokuapp.com/");
+
+        driver.findElement(By.linkText("Add/Remove Elements")).click();
+        WebElement title = driver.findElement(By.cssSelector("h3"));
+        Assert.assertEquals(title.getText(), "Add/Remove Elements");
+
+        driver.quit();
+    }
+
+    @Test
+    public void testHerokuappAddElements(){
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to("https://the-internet.herokuapp.com/add_remove_elements/");
+        driver.findElement(By.tagName("button")).click();
+        List<WebElement> elements = driver.findElements(By.xpath("//button[@class='added-manually']"));
+        Assert.assertEquals(elements.size(), 1);
 
         driver.quit();
     }
