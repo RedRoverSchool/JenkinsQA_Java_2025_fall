9353f09c8dbdfc1f6d1bf0491ddafde6fd6ef638
Igor <osinus-sb@ya.ru>
2025-10-19 00:45:37 +0700

    Delete the personal test Add the test in the group test

--- src/test/java/school/redrover/GroupJavaCrackersRedroverTest.java
+++ src/test/java/school/redrover/GroupJavaCrackersRedroverTest.java
@@ -160,4 +160,31 @@ public class GroupJavaCrackersRedroverTest {
 
         driver.quit();
     }
+
+
+    @Test
+    public void testCjCity() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://www.cjcity.ru");
+
+        WebElement button = driver.findElement(By.xpath("//*[@id=\"nav\"]/li[5]/a"));
+        button.click();
+
+        WebElement input = driver.findElement(By.xpath("//*[@id=\"content\"]/form/fieldset/div/div[1]/input"));
+        input.sendKeys("Luminoforium");
+
+        WebElement button1 = driver.findElement(By.xpath("//*[@id=\"content\"]/form/fieldset/div/div[3]/input"));
+        button1.click();
+
+        WebElement button2 = driver.findElement(By.xpath("//*[@id=\"content\"]/ul[2]/li/div[2]/span/a"));
+        button2.click();
+
+        WebElement img = driver.findElement(By.xpath("//*[@id=\"sidebar\"]/div[1]/div/img"));
+        Assert.assertEquals(img.getSize().getWidth(), 99);
+
+        driver.quit();
+
+    }
+
 }
