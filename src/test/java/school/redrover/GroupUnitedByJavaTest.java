7fd516f37615ee8719ee7354b634c28ddcc2a3e6
anastasiasusko <styu950@gmail.com>
2025-10-19 18:59:10 +0300

    Anastasiya Test Merge

--- src/test/java/school/redrover/GroupUnitedByJavaTest.java
+++ src/test/java/school/redrover/GroupUnitedByJavaTest.java
@@ -1084,5 +1084,47 @@ public class GroupUnitedByJavaTest {
         driver.quit();
     }
 
+    @Test
+    public void testTea () {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://roast.by/");
+
+        WebElement submitButton = driver.findElement(By.xpath("/html/body/div[2]/header[2]/div/div/div/div[3]/div/ul/li[2]/a/span"));
+        submitButton.click();
+
+        WebElement tea = driver.findElement(By.xpath("//*[@id='catalog-ics__list-js']/a[8]"));
+        tea.click();
+
+        WebElement message = driver.findElement(By.xpath("//*[@id='bx_1527614749_20308']/div/div/div[2]/a"));
+
+        Assert.assertEquals(message.getText(), "Фэн Хуан Дань Цун");
+        driver.quit();
+    }
+
+
+    @Test
+    public void testTeaCart () {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1920, 1080));
+        driver.get("https://roast.by/");
+
+        WebElement submitButton = driver.findElement(By.xpath("/html/body/div[2]/header[2]/div/div/div/div[3]/div/ul/li[2]/a/span"));
+        submitButton.click();
+
+        WebElement tea = driver.findElement(By.xpath("//*[@id='catalog-ics__list-js']/a[8]"));
+        tea.click();
+
+        WebElement cart = driver.findElement(By.xpath("//*[@id='bx_1527614749_20308']/div/div/div[3]/div/div[2]/div[1]"));
+        cart.click();
+
+        WebElement accs = driver.findElement(By.xpath("/html/body/div[2]/header[2]/div/div/div/div[3]/div/ul/li[3]/a/span"));
+        accs.click();
+
+        driver.quit();
+    }
+
 }
 
