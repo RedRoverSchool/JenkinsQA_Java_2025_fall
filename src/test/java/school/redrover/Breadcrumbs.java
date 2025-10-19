44fceac0f81213032f93bac2d9e9254912897fe6
SvitlanaNev <sidzp2019@gmail.com>
2025-10-19 19:37:33 +0300

    Delete class. Add test in group-class

--- src/test/java/school/redrover/Breadcrumbs.java
+++ src/test/java/school/redrover/Breadcrumbs.java
@@ -176,4 +176,76 @@ public class Breadcrumbs {
         Assert.assertEquals(driver.findElement(By.cssSelector(".signup-form > form > :nth-child(5)")).getText(), "Email Address already exist!");
         driver.quit();
     }
+
+    @Test
+    public void testSauseDemoVisualUser() {
+
+        WebDriver drive = new ChromeDriver();
+        drive.get("https://www.saucedemo.com/");
+
+        Assert.assertEquals(drive.getTitle(), "Swag Labs");
+
+        drive.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement userName = drive.findElement(By.id("user-name"));
+        userName.sendKeys("visual_user");
+
+        WebElement passw = drive.findElement(By.id("password"));
+        passw.sendKeys("secret_sauce");
+
+        WebElement submitButton = drive.findElement(By.name("login-button"));
+        submitButton.click();
+
+        WebElement page = drive.findElement(By.className("title"));
+
+        Assert.assertEquals(page.getText(), "Products");
+
+        drive.quit();
+    }
+
+    @Test
+    public void testSauceDemoStandardUser() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.getTitle();
+
+        WebElement usName = driver.findElement(By.id("user-name"));
+        WebElement pass = driver.findElement(By.id("password"));
+        WebElement login = driver.findElement(By.id("login-button"));
+
+        usName.sendKeys("standard_user");
+        pass.sendKeys("secret_sauce");
+        login.click();
+
+        WebElement page = driver.findElement(By.className("title"));
+
+        Assert.assertEquals(page.getText(), "Products");
+
+        driver.quit();
+    }
+
+    @Test
+    public void testNegativeLogin() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.getTitle();
+
+        WebElement usName = driver.findElement(By.id("user-name"));
+        WebElement pass = driver.findElement(By.id("password"));
+        WebElement login = driver.findElement(By.id("login-button"));
+
+        usName.sendKeys("error_user");
+        pass.sendKeys("passworddd");
+        login.click();
+
+        WebElement messageError =
+                driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3"));
+
+        Assert.assertEquals(messageError.getText(),
+                "Epic sadface: Username and password do not match any user in this service");
+
+
+    }
 }
