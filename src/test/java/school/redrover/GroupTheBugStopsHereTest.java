df6ee59a8cd6dbe149ab405a9aa1a9f225f148b8
RinatStr102 <156239446+RinatStr102@users.noreply.github.com>
2025-10-20 21:13:29 +0500

    Добавляю свой тест в общий класс (#273)

--- src/test/java/school/redrover/GroupTheBugStopsHereTest.java
+++ src/test/java/school/redrover/GroupTheBugStopsHereTest.java
@@ -210,4 +210,22 @@ public class GroupTheBugStopsHereTest {
             System.out.println("ℹ️ No cookie consent dialog found. Continuing test.");
         }
     }
+
+    @Test
+    public void testLoginWithValidCredentials() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.automationexercise.com/");
+
+        driver.findElement(By.xpath("//button[@aria-label='Соглашаюсь']")).click();
+
+        Assert.assertEquals(driver.getTitle(), "Automation Exercise");
+
+        driver.findElement(By.xpath("//a[@href='/login']")).click();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//h2[text()='Login to your account']")).isDisplayed());
+
+        driver.quit();
+
+    }
 }
