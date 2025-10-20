0eb9c9ed1dbd232d3ae711b482993bf6e2531e88
M-Ostapinskiyy <ostapinskiyy@gmail.com>
2025-10-20 10:12:18 +0300

    rename Breadcrumbs to BreadcrumbsTest

--- src/test/java/school/redrover/BreadcrumbsTest.java
+++ src/test/java/school/redrover/BreadcrumbsTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class Breadcrumbs {
+public class BreadcrumbsTest {
 
     @Test
     public void mikhailOtest() throws InterruptedException {
