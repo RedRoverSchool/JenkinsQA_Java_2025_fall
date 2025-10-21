3ccb201fc8aa2e87b5015e5e8ce2ec6c18cd442c
Lev Eydelkind <eydelkindl@gmail.com>
2025-10-21 22:55:31 +0300

    GroupApexTeamTest created LevEydelkindTest deleted

--- src/test/java/school/redrover/GroupApexTeamTest.java
+++ src/test/java/school/redrover/GroupApexTeamTest.java
@@ -6,9 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
-public class LevEydelkindTest {
+public class GroupApexTeamTest {
 
     @Test
     public void sauceDemo (){
