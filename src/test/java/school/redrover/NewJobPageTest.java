bc24de4c45b51b97021dc54a2f0f5282331fd98d
Nat P <natashaadam.p43@gmail.com>
2025-10-24 11:47:02 +0200

    changed locators to shorter and clearer ones

--- src/test/java/school/redrover/NewJobPageTest.java
+++ src/test/java/school/redrover/NewJobPageTest.java
@@ -10,8 +10,8 @@ public class NewJobPageTest extends BaseTest {
     @Test
     public  void testItemNameEmpty(){
 
-        getDriver().findElement(By.cssSelector("#tasks > div:nth-child(1) a")).click();
-        getDriver().findElement((By.xpath("//*[@id='j-add-item-type-nested-projects']/ul/li[1]"))).click();
+        getDriver().findElement(By.cssSelector("#tasks a")).click();
+        getDriver().findElement((By.cssSelector("[class$='OrganizationFolder']"))).click();
 
         Assert.assertEquals(getDriver().findElement(By.id("itemname-required")).getText(), "» This field cannot be empty, please enter a valid name");
         Assert.assertFalse(getDriver().findElement(By.id("ok-button")).isEnabled());
