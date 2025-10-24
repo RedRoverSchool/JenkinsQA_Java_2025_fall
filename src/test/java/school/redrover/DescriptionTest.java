740e26e1e0343b8538b950ea63d11c32f646f7c8
Dariya Zubovich <dariyazubovich@gmail.com>
2025-10-25 00:10:24 +0300

    Add DescriptionTest (#311)
    * Add DescriptionTest
    
    * Delete empty string

---
+++ src/test/java/school/redrover/DescriptionTest.java
@@ -0,0 +1,23 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class DescriptionTest extends BaseTest {
+
+    @Test
+    public void testCreateDescription() throws InterruptedException {
+        final String description = "Test description";
+
+        getDriver().findElement(By.id("description-link")).click();
+        getDriver().findElement(By.name("description")).sendKeys(description);
+        getDriver().findElement(By.name("Submit")).click();
+
+        Thread.sleep(2000);
+
+        String actualDescription = getDriver().findElement(By.id("description-content")).getText();
+        Assert.assertEquals(actualDescription, description);
+    }
+}
