7c5fbf95fdf1a82ac5fedced87163dfc1d5c7aa7
Elena-Galina <e.lena.galina@yandex.ru>
2025-10-24 20:54:59 +0300

    eg/add tests for NewItemPage-v2

--- src/test/java/school/redrover/NewItemPageTest.java
+++ src/test/java/school/redrover/NewItemPageTest.java
@@ -12,13 +12,12 @@ import java.time.Duration;
 import java.util.List;
 
 public class NewItemPageTest extends BaseTest {
-    private final By newItemButtonFromHomePage = By.xpath(".//div[@id=\"tasks\"]/div[1]/span/a");
+    private final By newItemButtonFromHomePage = By.xpath(".//div[@id='tasks']/div[1]/span/a");
 
-    public void newItemButtonClick() {
+    private void newItemButtonClick() {
         WebElement newItemButton = new WebDriverWait(getDriver(), Duration.ofSeconds(5))
                 .until(ExpectedConditions.visibilityOfElementLocated(newItemButtonFromHomePage));
 
-        Assert.assertEquals(newItemButton.getText(), "New Item");
         getDriver().findElement(newItemButtonFromHomePage).click();
     }
 
@@ -29,13 +28,13 @@ public class NewItemPageTest extends BaseTest {
         Assert.assertEquals(
                 getDriver().findElement(By.xpath(".//h1")).getText(), "New Item");
 
-        WebElement itemNameLabel = getDriver().findElement(By.xpath(".//label[@class=\"jenkins-form-label\"]"));
+        WebElement itemNameLabel = getDriver().findElement(By.xpath(".//label[@class='jenkins-form-label']"));
         Assert.assertEquals(itemNameLabel.getText(), "Enter an item name");
 
-        WebElement itemNameField = getDriver().findElement(By.xpath(".//*[@id=\"name\"]"));
-        Assert.assertTrue(itemNameField.isEnabled(), "Поле \"Enter an item name\" должно быть доступно для ввода");
+        WebElement itemNameField = getDriver().findElement(By.xpath(".//*[@id='name']"));
+        Assert.assertTrue(itemNameField.isEnabled(), "Поле 'Enter an item name' должно быть доступно для ввода");
 
-        WebElement itemTypeLabel = getDriver().findElement(By.xpath(".//div[@class=\"jenkins-form-label\"]"));
+        WebElement itemTypeLabel = getDriver().findElement(By.xpath(".//div[@class='jenkins-form-label']"));
         Assert.assertEquals(itemTypeLabel.getText(), "Select an item type");
     }
 
@@ -44,7 +43,7 @@ public class NewItemPageTest extends BaseTest {
         newItemButtonClick();
 
         List<String> expectedItemTypes = List.of("Freestyle project", "Pipeline", "Multi-configuration project", "Folder", "Multibranch Pipeline", "Organization Folder");
-        List<WebElement> itemTypes = getDriver().findElements(By.xpath(".//span[@class=\"label\"]"));
+        List<WebElement> itemTypes = getDriver().findElements(By.xpath(".//span[@class='label']"));
 
         List<String> itemTypeList = itemTypes.stream()
                 .map(WebElement::getText)
