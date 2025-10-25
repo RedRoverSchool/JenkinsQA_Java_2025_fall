1647f1fecd36d9cfba64bb6c2fd66bbf5de4df7a
LiliaA <samohinalilia93@gmail.com>
2025-10-25 15:23:38 +0300

    added tests for pipelines

---
+++ src/test/java/school/redrover/PipelineTest.java
@@ -0,0 +1,55 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+import java.util.List;
+
+public class PipelineTest extends BaseTest {
+
+    final String pipelineName = "PipelineName";
+
+    @Test
+    public void testCreatePipeline() throws InterruptedException {
+        getDriver().findElement(By.cssSelector(".task:nth-child(1) a")).click();
+        getDriver().findElement(By.cssSelector("#name")).sendKeys(pipelineName);
+        getDriver().findElement(By.cssSelector("div:first-child > ul > li:nth-child(2)")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+
+        Thread.sleep(2000);
+        getDriver().findElement(By.xpath("//a[@href='/']/img")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//a[@href='job/" + pipelineName + "/']")).getText(),
+                pipelineName);
+    }
+
+    @Test
+    public void testDeletePipeline() throws InterruptedException {
+        Actions actions = new Actions(getDriver());
+
+        getDriver().findElement(By.cssSelector(".task:nth-child(1) a")).click();
+        getDriver().findElement(By.cssSelector("#name")).sendKeys(pipelineName);
+        getDriver().findElement(By.cssSelector("div:first-child > ul > li:nth-child(2)")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+
+        Thread.sleep(2000);
+        getDriver().findElement(By.xpath("//a[@href='/']/img")).click();
+
+        List<WebElement> countPosition = getDriver().findElements(By.cssSelector("#projectstatus > tbody > tr"));
+
+        actions.moveToElement(getDriver().findElement(By.xpath("//a[@href='job/" + pipelineName + "/']"))).perform();
+        getDriver().findElement(By.xpath("//a[@href='job/" + pipelineName + "/']/button")).click();
+
+        Thread.sleep(2000);
+        getDriver().findElement(By.cssSelector(".jenkins-dropdown__item:nth-child(4)")).click();
+        getDriver().findElement(By.xpath("//button[@data-id='ok']")).click();
+
+        Assert.assertEquals(countPosition.size() - 1, 0);
+    }
+}
