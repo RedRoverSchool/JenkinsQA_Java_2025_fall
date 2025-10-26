6d375289571ba81b30843402fe130bfd36f7f660
Roman Burlaka <burlakarv@gmail.com>
2025-10-26 16:51:38 +0100

    multionfigurationrojectest -> testddescriptionoroject

--- src/test/java/school/redrover/MultiConfigurationProjectTest.java
+++ src/test/java/school/redrover/MultiConfigurationProjectTest.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -46,6 +47,23 @@ public class MultiConfigurationProjectTest extends BaseTest {
         return getDriver().findElement(By.xpath("//h1[@class= 'matrix-project-headline page-headline']")).getText();
     }
 
+    public void goToProject(String projectName) {
+        getDriver().findElement(By.xpath(String.format("//td/a[@href='job/%s/']", projectName))).click();
+
+    }
+
+    public void goToDashBoard() {
+        waitTime(30).until(ExpectedConditions.visibilityOfElementLocated((By.className("app-jenkins-logo"))))
+                .click();
+    }
+
+    public void renameWithSidePanel(String newName) {
+        getDriver().findElement(By.xpath("(//div[@id='side-panel']//div[@class='task '])[7]")).click();
+        WebElement rename = getDriver().findElement(By.name("newName"));
+        rename.clear();
+        rename.sendKeys(newName, Keys.ENTER);
+    }
+
     @Test
     public void testCreateProject() {
         createNewJob();
@@ -57,6 +75,33 @@ public class MultiConfigurationProjectTest extends BaseTest {
         softAssert.assertEquals(getTitleOfProject(), NAME_OF_PROJECT);
         softAssert.assertTrue(waitTime(20).until(ExpectedConditions.urlContains(NAME_OF_PROJECT)));
         softAssert.assertAll();
+    }
+
+    @Test(priority = 1)
+    public void testAddDescriptionToProject() {
+        createNewJob();
+        setNameOfProject(NAME_OF_PROJECT);
+        selectProject();
+        submitCreateProject();
+        submitConfigure();
+        goToDashBoard();
+        goToProject(NAME_OF_PROJECT);
+        renameWithSidePanel("NewNameProject");
+        getTitleOfProject();
+
+        softAssert.assertEquals(getTitleOfProject(), "NewNameProject");
+    }
+
+    @Test(priority = 2)
+    public void testRenameProject() {
+        //your code may be here
+
+    }
+
+    @Test(priority = 3)
+    public void testDeleteProject() {
+        //your code may be here
+
     }
 
 
