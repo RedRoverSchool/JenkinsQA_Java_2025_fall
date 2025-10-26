82b5f254a11255ccd1f857eedb29e56bd9606514
Stanislav Agafonov <chaltstas@yandex.ru>
2025-10-26 16:20:35 +0300

    add testSuccessfulBuildPipeline and method createPipeline

--- src/test/java/school/redrover/PipelineTest.java
+++ src/test/java/school/redrover/PipelineTest.java
@@ -12,6 +12,21 @@ public class PipelineTest extends BaseTest {
 
     private static final String PIPELINE_NAME = "PipelineName";
 
+    /**
+     * Создает новый Pipeline с именем {@link #PIPELINE_NAME}.
+     * И переходит на страницу конфигурации job (т.е. Остается на главной странице задания после создания).
+     * Не возвращается на панель управления Jenkins.
+     */
+    private void createPipeline() {
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+
+        getDriver().findElement(By.id("name")).sendKeys(PIPELINE_NAME);
+        getDriver().findElement(By.className("org_jenkinsci_plugins_workflow_job_WorkflowJob")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
+    }
+
     @Test
     public void testCreatePipeline() throws InterruptedException {
         getDriver().findElement(By.cssSelector(".task:nth-child(1) a")).click();
@@ -46,4 +61,21 @@ public class PipelineTest extends BaseTest {
 
         Assert.assertEquals(countPosition.size() - 1, 0);
     }
+
+    @Test
+    public void testSuccessfulBuildPipeline() throws InterruptedException {
+
+        createPipeline();
+
+        getDriver().findElement(By.xpath("//a[@data-build-success='Build scheduled']")).click();
+
+        Thread.sleep(3000);
+        getDriver().findElement(By.id("jenkins-build-history")).click();
+        getDriver().findElement(By.xpath("//a[substring-before(@href, 'console')]")).click();
+
+        String consoleOutputText = getDriver().findElement(By.id("consoleOutputText")).getText();
+
+        Assert.assertTrue(consoleOutputText.contains("Finished: SUCCESS"), "Build output should contain 'Finished: SUCCESS'");
+    }
+
 }
