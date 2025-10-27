b94d0a1f746c749994ecf818370dd7ff1cba7217
Denis Lafa <denislafa@gmail.com>
2025-10-27 15:02:29 +0300

    Add available job types test

---
+++ src/test/java/school/redrover/AvailableJobTypesTest.java
@@ -0,0 +1,38 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+import java.time.Duration;
+import java.util.List;
+
+public class AvailableJobTypesTest extends BaseTest {
+
+    @Test
+    public void testJobTypesAreDisplayed() {
+
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[@class='jenkins-form-label']")));
+
+        List<String> expectedItemType = List.of(
+                "Pipeline",
+                "Folder",
+                "Multibranch Pipeline",
+                "Organization Folder"
+        );
+
+        List<String> actualItemTypes = getDriver().findElements(By.xpath("//div[@id='items']//label"))
+                .stream()
+                .map(WebElement::getText)
+                .filter(expectedItemType::contains)
+                .toList();
+
+        Assert.assertEquals(actualItemTypes, expectedItemType);
+    }
+}
