4626fe9e16016f91a7abdc583b91884851d9c9af
UlianaScolnicova <yliana.tol@gmail.com>
2025-10-27 21:00:32 +0200

    testLogo

---
+++ src/test/java/school/redrover/LogoTest.java
@@ -0,0 +1,24 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class LogoTest extends BaseTest {
+
+    @Test
+    public void testLogo() {
+
+        WebElement logo = getDriver().findElement(By.className("app-jenkins-logo"));
+
+        String logoText = logo.findElement(By.xpath(".//span[@class='jenkins-mobile-hide']")).getText();
+
+        logo.click();
+
+        Assert.assertEquals(logoText, "Jenkins", "Надпись рядом с логотипом должна быть 'Jenkins'");
+        Assert.assertEquals(getDriver().getCurrentUrl(), "http://localhost:8080/",
+                "URL после клика по логотипу должен быть http://localhost:8080/");
+    }
+}
