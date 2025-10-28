a8303172d3a9badc9819aef06c2be5db16e99bc8
Roman Burlaka <burlakarv@gmail.com>
2025-10-28 20:14:29 +0100

    deleted temporary comment

--- src/test/java/school/redrover/FreestyleProjectTest.java
+++ src/test/java/school/redrover/FreestyleProjectTest.java
@@ -32,24 +32,4 @@ public class FreestyleProjectTest extends BaseTest {
 
         Assert.assertEquals(projectTitle.getText(), "Freestyle Project");
     }
-
-    @Test
-    public void testRenameProjectTest() throws InterruptedException {
-        WebElement createJob = getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']"));
-        createJob.click();
-        WebElement projectName = getDriver().findElement(By.id("name"));
-        projectName.sendKeys("Freestyle Project");
-        WebElement freestyleProjectOption = getDriver().findElement(By.xpath("//span[text()='Freestyle project']"));
-        freestyleProjectOption.click();
-        WebElement okButton = getDriver().findElement(By.id("ok-button"));
-        okButton.click();
-        WebElement saveButton = getDriver().findElement(By.name("Submit"));
-        saveButton.click();
-        Thread.sleep(1500);
-        WebElement projectTitle = getDriver().findElement(By.xpath("//h1"));
-
-
-
-    }
-
 }
