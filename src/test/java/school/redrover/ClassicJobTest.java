74e97021543c06a632dae18e9834af6cd48bca55
Denis Lafa <denislafa@gmail.com>
2025-10-28 06:42:33 +0300

    Add new test to ClassicJobTest

--- src/test/java/school/redrover/ClassicJobTest.java
+++ src/test/java/school/redrover/ClassicJobTest.java
@@ -8,6 +8,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 import java.time.Duration;
+import java.util.List;
 
 
 public class ClassicJobTest extends BaseTest {
@@ -32,5 +33,29 @@ public class ClassicJobTest extends BaseTest {
 
         WebElement createdJobLink = getDriver().findElement(By.xpath("//a[@class='jenkins-table__link model-link inside']"));
         Assert.assertEquals(createdJobLink.getText(), jobName);
+    }
+
+    @Test
+    public void testJobTypesAreDisplayed() {
+
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[@class='jenkins-form-label']")));
+
+        List<String> expectedItemType = List.of(
+                "Pipeline",
+                "Folder",
+                "Multibranch Pipeline",
+                "Organization Folder"
+        );
+
+        List<String> actualItemTypes = getDriver().findElements(By.xpath("//div[@id='items']//label"))
+                .stream()
+                .map(WebElement::getText)
+                .filter(expectedItemType::contains)
+                .toList();
+
+        Assert.assertEquals(actualItemTypes, expectedItemType);
     }
 }
