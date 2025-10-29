4454a135b20523126ae1d7c216b785615d2c75c9
Alexey Vorobyev <118996115+AlexeiVorobyev@users.noreply.github.com>
2025-10-29 15:04:12 -0700

    Av/ New Class with Test (#366)
    * Create a new class and test to check all items is displayed
    
    * Create a new class and test to check all items is displayed

---
+++ src/test/java/school/redrover/CreateNewItem1Test.java
@@ -0,0 +1,33 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class CreateNewItem1Test extends BaseTest {
+
+    @Test
+    public void testItemsAreDisplayed() {
+
+        String[] expectedItemsList = {
+                "Freestyle project",
+                "Pipeline",
+                "Multi-configuration project",
+                "Folder",
+                "Multibranch Pipeline",
+                "Organization Folder"
+        };
+
+        String[] actualItemsList = new String[expectedItemsList.length];
+
+        getDriver().findElement(By.xpath("//span[text()='Create a job']")).click();
+
+        for (int i = 0; i < expectedItemsList.length; i++) {
+            actualItemsList[i] = getDriver().findElement(By.xpath("//span[text()='%s']".formatted(expectedItemsList[i]))).getText();
+        }
+
+        Assert.assertEquals(actualItemsList,expectedItemsList);
+
+    }
+}
