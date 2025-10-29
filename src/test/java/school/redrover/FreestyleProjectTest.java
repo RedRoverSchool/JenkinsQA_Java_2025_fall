a016f0a97239694aaa486959e792111bca6a0ade
Denis Prokofyev <denisprokofyev@gmail.com>
2025-10-29 14:35:08 +0300

    Add Ignore to testAddDescription

--- src/test/java/school/redrover/FreestyleProjectTest.java
+++ src/test/java/school/redrover/FreestyleProjectTest.java
@@ -3,6 +3,7 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
@@ -33,6 +34,7 @@ public class FreestyleProjectTest extends BaseTest {
         Assert.assertEquals(projectTitle.getText(), "Freestyle Project");
     }
 
+    @Ignore
     @Test
     public void testAddDescription() {
         WebElement createJob = getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']"));
