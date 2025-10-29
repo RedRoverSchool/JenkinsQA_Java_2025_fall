ca3d165838e7873cdcb093bb052cdb5f0e6c3fc3
SergeyD <Sergei.Demyanenko@gmail.com>
2025-10-29 01:19:02 -0400

    Add Folder test (#352)

--- src/test/java/school/redrover/Folder1Test.java
+++ src/test/java/school/redrover/Folder1Test.java
@@ -2,9 +2,12 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
+
+import java.util.List;
 
 public class Folder1Test extends BaseTest {
 
@@ -25,5 +28,40 @@ public class Folder1Test extends BaseTest {
 
         WebElement jobRecordCell = getDriver().findElement(By.cssSelector(".jenkins-table__link >span:first-child"));
         Assert.assertEquals(jobRecordCell.getText(), folderName);
+    }
+
+    @Test
+    public void testPutFolderToFolder() throws InterruptedException {
+        final String folder1Name = "folder1";
+        final String folder2Name = "folder2";
+
+        // create folder1
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(folder1Name);
+        getDriver().findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        Thread.sleep(2000);
+        getDriver().findElement(By.cssSelector("span.jenkins-mobile-hide")).click();
+
+        // create folder2
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(folder2Name);
+        getDriver().findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        Thread.sleep(2000);
+        getDriver().findElement(By.cssSelector("span.jenkins-mobile-hide")).click();
+
+        getDriver().findElement(By.xpath("//a[@href='job/%s/']".formatted(folder2Name))).click();
+        getDriver().findElement(By.xpath("//a[@href='/job/%s/move']".formatted(folder2Name))).click();
+
+        Select selectObject = new Select(getDriver().findElement(By.className("jenkins-select__input")));
+        selectObject.selectByVisibleText("Jenkins » %s".formatted(folder1Name));
+
+        getDriver().findElement(By.name("Submit")).click();
+        Thread.sleep(2000);
+
+        List<WebElement> list = getDriver().findElements(By.cssSelector(".jenkins-breadcrumbs__list-item > a"));
+        Assert.assertEquals(list.get(0).getText(), folder1Name);
+        Assert.assertEquals(list.get(1).getText(), folder2Name);
     }
 }
