afc1adc39ef19d27d34361956c0b1b291e1c52ef
LiliaA <samohinalilia93@gmail.com>
2025-10-30 21:03:27 +0300

    added testSignOutByClickUserMenu

---
+++ src/test/java/school/redrover/SignInOutTest.java
@@ -0,0 +1,21 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class SignInOutTest extends BaseTest {
+
+    @Test
+    public void testSignOutByClickUserMenu() {
+
+        Actions actions = new Actions(getDriver());
+
+        actions.moveToElement(getDriver().findElement(By.id("root-action-UserAction"))).perform();
+        getDriver().findElement(By.cssSelector(".jenkins-dropdown__item:last-child")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.tagName("h1")).getText(), "Sign in to Jenkins");
+    }
+}
