dafc1074a57165e2688669d9121c54869ac150e4
xkaktyzx <xkaktyzx1@gmail.com>
2025-10-30 17:22:57 +0100

    Add checking for empty input fields test

--- src/test/java/school/redrover/CreateUserTest.java
+++ src/test/java/school/redrover/CreateUserTest.java
@@ -1,9 +1,13 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
+
+import java.time.Duration;
+import java.util.List;
 
 
 public class CreateUserTest extends BaseTest {
@@ -25,5 +29,30 @@ public class CreateUserTest extends BaseTest {
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//td[text()='%s']".formatted(userName))).getText(),
                 userName);
+    }
+
+    @Test
+    public void testCheckingEmptyInput() {
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        final List<String> expectedErrors = List.of(
+                "\"\" is prohibited as a username for security reasons.",
+                "Требуется пароль",
+                "Требуется пароль",
+                "\"\" is prohibited as a full name for security reasons.",
+                "Некорректный e-mail"
+        );
+        getDriver().findElement(By.id("root-action-ManageJenkinsAction")).click();
+        getDriver().findElement(By.xpath("//dt[text()='Users']")).click();
+        getDriver().findElement(By.className("jenkins-button--primary")).click();
+        getDriver().findElement(By.name("Submit")).click();
+
+
+        List<String> actualErrors = getDriver()
+                .findElements(By.xpath("//*[@class='error jenkins-!-margin-bottom-2']"))
+                .stream()
+                .map(WebElement::getText)
+                .toList();
+
+        Assert.assertEquals(expectedErrors, actualErrors);
     }
 }
