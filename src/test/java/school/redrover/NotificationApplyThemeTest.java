013e28d48bf418959d8cbdeefee715d399c8fb1e
TinaSilver-fox <tinaisenka@gmail.com>
2025-10-31 03:19:22 +0300

    Fix xPath

--- src/test/java/school/redrover/NotificationApplyThemeTest.java
+++ src/test/java/school/redrover/NotificationApplyThemeTest.java
@@ -11,7 +11,7 @@ public class NotificationApplyThemeTest extends BaseTest {
     @Test
     public void testChangeTheme() throws InterruptedException {
         getDriver().findElement(By.id("root-action-UserAction")).click();
-        getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[5]/span/a")).click();
+        getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[5]/span/a/span[1]")).click();
         getDriver().findElement(By.xpath("//*[@id=\"main-panel\"]/form/div[1]/section/div[4]/div[1]/div/div[2]/div[1]/div/label")).click();
         getDriver().findElement(By.className("apply-button")).click();
         WebElement result = getDriver().findElement(By.className("jenkins-notification--success"));
