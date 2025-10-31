37ef13d7ac44a09fcb53dbb6ac227ea859b0a3ee
TinaSilver-fox <tinaisenka@gmail.com>
2025-10-31 03:01:41 +0300

    Apply Theme test

---
+++ src/test/java/school/redrover/NotificationApplyThemeTest.java
@@ -0,0 +1,21 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class NotificationApplyThemeTest extends BaseTest {
+
+    @Test
+    public void testChangeTheme() throws InterruptedException {
+        getDriver().findElement(By.id("root-action-UserAction")).click();
+        getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[5]/span/a")).click();
+        getDriver().findElement(By.xpath("//*[@id=\"main-panel\"]/form/div[1]/section/div[4]/div[1]/div/div[2]/div[1]/div/label")).click();
+        getDriver().findElement(By.className("apply-button")).click();
+        WebElement result = getDriver().findElement(By.className("jenkins-notification--success"));
+        Assert.assertEquals(result.getText(), "Saved");
+    }
+
+}
