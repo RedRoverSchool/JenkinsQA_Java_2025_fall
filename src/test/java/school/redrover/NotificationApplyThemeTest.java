a64e115b246907f5d47af1d1cec94c6a70997b8c
TinaSilver-fox <tinaisenka@gmail.com>
2025-10-31 23:37:53 +0300

    Fix

--- src/test/java/school/redrover/NotificationApplyThemeTest.java
+++ src/test/java/school/redrover/NotificationApplyThemeTest.java
@@ -1,10 +1,16 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
+
+import java.time.Duration;
 
 public class NotificationApplyThemeTest extends BaseTest {
 
@@ -12,7 +18,9 @@ public class NotificationApplyThemeTest extends BaseTest {
     public void testChangeTheme() throws InterruptedException {
         getDriver().findElement(By.id("root-action-UserAction")).click();
         Thread.sleep(2000);
-        getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[5]/span/a/span[1]")).click();
+        Wait<WebDriver> wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"tasks\"]/div[5]/span")));
+        element.click();
         getDriver().findElement(By.xpath("//*[@id=\"main-panel\"]/form/div[1]/section/div[4]/div[1]/div/div[2]/div[1]/div/label")).click();
         getDriver().findElement(By.className("apply-button")).click();
         Thread.sleep(1000);
