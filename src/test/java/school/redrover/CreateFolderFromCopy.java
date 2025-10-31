1e15e165df1a0be9622c7d6088e297aef0d8845b
gaitanchristina92 <gaitanchristina@gmail.com>
2025-10-31 21:48:45 +0300

    Creating a copy of a folder (including creating the original folder)

--- src/test/java/school/redrover/CreateFolderFromCopy.java
+++ src/test/java/school/redrover/CreateFolderFromCopy.java
@@ -10,25 +10,20 @@ import school.redrover.common.BaseTest;
 public class CreateFolderFromCopy extends BaseTest {
 
     @Test
-    public void createFolderTest() {
-
-        final String fileName = "MyFirstFolder";
-        final String displayName = "Name for Autotest attempt";
-        final String description = "Description for Autotest attempt";
-        final String textCopyFrom = "Copy from";
+    public void createFolderAndCopyItem() {
 
         //First create the folder
 
         getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys(fileName);
+        getDriver().findElement(By.id("name")).sendKeys("MyFirstFolder");
         getDriver().findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder")).click();
 
         WebElement okButton = getDriver().findElement(By.id("ok-button"));
         ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);", okButton);
         okButton.click();
 
-        getDriver().findElement(By.xpath("//div[@class='setting-main']//input")).sendKeys(displayName);
-        getDriver().findElement(By.xpath("//div[@class='setting-main']//textarea")).sendKeys(description);
+        getDriver().findElement(By.xpath("//div[@class='setting-main']//input")).sendKeys("Name for Autotest attempt");
+        getDriver().findElement(By.xpath("//div[@class='setting-main']//textarea")).sendKeys("Description for Autotest attempt");
         WebElement submitButton = getDriver().findElement(By.className("jenkins-submit-button"));
         submitButton.click();
 
@@ -38,9 +33,7 @@ public class CreateFolderFromCopy extends BaseTest {
         newItemButton.click();
 
         ((JavascriptExecutor) getDriver()).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-       Assert.assertEquals(getDriver().findElement(By.className("add-item-copy")).getText(), textCopyFrom);
-
+       Assert.assertEquals(getDriver().findElement(By.className("add-item-copy")).getText(), "Copy from");
 
     }
-
 }
