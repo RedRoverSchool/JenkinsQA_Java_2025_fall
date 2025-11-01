6be78a4c220034f26485d12ac8e296b0edecaef7
alexrudak <rudakalexx@gmail.com>
2025-11-01 17:07:15 +0100

    disableAfterCreationTest

--- src/test/java/school/redrover/MultiConfigurationProjectEnabledTest.java
+++ src/test/java/school/redrover/MultiConfigurationProjectEnabledTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
@@ -15,8 +16,26 @@ public class MultiConfigurationProjectEnabledTest extends BaseTest {
     private final String projectName = "NewMulti-ConfigurationProject";
 
     @Test
+    void disableAfterCreationTest() throws InterruptedException {
+        createMCProject(projectName);
+        Thread.sleep(500);
+        getDriver().findElement(By.xpath("//label[@class='jenkins-toggle-switch__label ']")).click();
+        Thread.sleep(500);
+
+        Assert.assertTrue(getDriver().findElement(By.
+                        xpath("//span[@class='jenkins-toggle-switch__label__unchecked-title']")).isDisplayed(),
+                "'Disabled' must be shown");
+        Assert.assertFalse(getDriver().findElement(By.
+                        xpath("//label[@class='jenkins-toggle-switch__label ']")).isSelected(),
+                "Toggle Button should be Disabled");
+    }
+
+    @Ignore
+    @Test
     void disableFromHomePageTest() throws InterruptedException {
         createMCProject(projectName);
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/']"))).click();
         Thread.sleep(500);
 
         WebElement findArrow = getDriver().findElement(By.
@@ -41,11 +60,9 @@ public class MultiConfigurationProjectEnabledTest extends BaseTest {
     }
 
     private void createMCProject(String name) {
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
         getDriver().findElement(By.xpath("//div[@class='task ']")).click();
         getDriver().findElement(By.xpath("//input[@name='name']")).sendKeys(name);
         getDriver().findElement(By.xpath("//li[@class='hudson_matrix_MatrixProject']")).click();
         getDriver().findElement(By.id("ok-button")).click();
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/']"))).click();
     }
 }
