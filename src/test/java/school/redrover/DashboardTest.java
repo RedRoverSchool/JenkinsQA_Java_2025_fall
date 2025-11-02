17eaaf484f607a479b66f393e1658715068b3d6d
StepanidaKirillina1 <steshakirillina@yandex.ru>
2025-11-03 08:38:31 +0900

    AT_11.001.04 | Welcome Dashboard > View Welcome Message and start page > Verify the paragraph text

--- src/test/java/school/redrover/DashboardTest.java
+++ src/test/java/school/redrover/DashboardTest.java
@@ -23,4 +23,14 @@ public class DashboardTest extends BaseTest {
 
         Assert.assertEquals(actualHeading.getText(), "Welcome to Jenkins!");
     }
+
+    @Test
+    public void testHomePageParagraph() {
+        final String expectedParagraph = "This page is where your Jenkins jobs will be displayed. " +
+                "To get started, you can set up distributed builds or start building a software project.";
+
+        WebElement actualParagraph = getWait().until(ExpectedConditions.visibilityOfElementLocated(By.tagName("p")));
+
+        Assert.assertEquals(actualParagraph.getText(), expectedParagraph);
+    }
 }
