731f94e4f58b194ed614cd44725e8c0f31f12bc1
anastasiasusko <styu950@gmail.com>
2025-11-02 23:22:03 +0300

    Disable New MultiConfig Project

---
+++ src/test/java/school/redrover/EnableDisableMultiConfigProjectTest.java
@@ -0,0 +1,22 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class EnableDisableMultiConfigProjectTest extends BaseTest {
+
+    @Test
+    public void disableNewMultiConfigProject () {
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys("NewProject");
+        getDriver().findElement(By.xpath("//li[@class='hudson_matrix_MatrixProject']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+
+        getDriver().findElement(By.xpath("//label[@for='enable-disable-project']")).click();
+
+        String actualState = "Disabled";
+        Assert.assertEquals(actualState, "Disabled");
+    }
+}
