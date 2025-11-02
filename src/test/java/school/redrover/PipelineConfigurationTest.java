33f7a7fb5fd9204ab1cc63b89a606f2d3d8b88ce
Yuliya Bezlatneva <bezlatneva@yandex.com>
2025-11-02 09:45:08 +0300

    refactor: Enable Disable project tests

--- src/test/java/school/redrover/PipelineConfigurationTest.java
+++ src/test/java/school/redrover/PipelineConfigurationTest.java
@@ -1,9 +1,8 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -12,39 +11,41 @@ import school.redrover.common.BaseTest;
 import java.time.Duration;
 
 public class PipelineConfigurationTest extends BaseTest {
+    private final By toggleSwitch = By.xpath("//span[@id='toggle-switch-enable-disable-project']");
+
+    private void createFreestyleProject(String name) {
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys(name);
+        getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']")).click();
+        getDriver().findElement(By.xpath("//*[@id='ok-button']")).click();
+    }
 
     @Test
     public void testEnableProject() {
-        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
-        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys("ChangeStatusEnabledTest");
-        getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']")).click();
-        getDriver().findElement(By.xpath("//*[@id='ok-button']")).click();
+        createFreestyleProject("ChangeStatusEnabledTest");
 
-        WebElement enableDisableToggle = getDriver().findElement(By.xpath("//span[@id='toggle-switch-enable-disable-project']"));
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(toggleSwitch));
 
-        Wait<WebDriver> wait = new WebDriverWait(getDriver(), Duration.ofSeconds(2));
-        wait.until(d -> enableDisableToggle.isDisplayed());
-
-        Assert.assertEquals(getDriver()
-                .findElement(By.xpath("//span[@class='jenkins-toggle-switch__label__checked-title']"))
-                .getText(), "Enabled");
+        String enabledText = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        By.className("jenkins-toggle-switch__label__checked-title")
+                )).getText();
+        Assert.assertEquals(enabledText, "Enabled");
     }
 
     @Test
     public void testDisableProject() {
-        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
-        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys("ChangeStatusDisabledTest");
-        getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']")).click();
-        getDriver().findElement(By.xpath("//*[@id='ok-button']")).click();
+        createFreestyleProject("ChangeStatusDisabledTest");
 
-        WebElement enableDisableToggle = getDriver().findElement(By.xpath("//span[@id='toggle-switch-enable-disable-project']"));
-
-        Wait<WebDriver> wait = new WebDriverWait(getDriver(), Duration.ofSeconds(2));
-        wait.until(d -> enableDisableToggle.isDisplayed());
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        WebElement enableDisableToggle = wait.until(ExpectedConditions.visibilityOfElementLocated(toggleSwitch));
         enableDisableToggle.click();
 
-        Assert.assertEquals(getDriver()
-                .findElement(By.xpath("//span[@class='jenkins-toggle-switch__label__unchecked-title']"))
-                .getText(), "Disabled");
+        String disabledText = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        By.className("jenkins-toggle-switch__label__unchecked-title")
+                )).getText();
+        Assert.assertEquals(disabledText, "Disabled");
     }
 }
