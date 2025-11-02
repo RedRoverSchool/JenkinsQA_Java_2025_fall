f07c6b079172e7550e8aca2f4da5da72d78dba7c
alexrudak <rudakalexx@gmail.com>
2025-11-02 20:42:46 +0100

    Test for 5th Acceptance Criteria

--- src/test/java/school/redrover/MultiConfigurationProjectEnabledTest.java
+++ src/test/java/school/redrover/MultiConfigurationProjectEnabledTest.java
@@ -1,9 +1,14 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
+
+import java.time.Duration;
 
 public class MultiConfigurationProjectEnabledTest extends BaseTest {
     private final String projectName = "NewMulti-ConfigurationProject";
@@ -21,6 +26,24 @@ public class MultiConfigurationProjectEnabledTest extends BaseTest {
         Assert.assertFalse(getDriver().findElement(By.
                         xpath("//label[@class='jenkins-toggle-switch__label ']")).isSelected(),
                 "Toggle Button should be Disabled");
+    }
+
+    @Test
+    void iconWhenDisableTest() throws InterruptedException {
+        createMCProject(projectName);
+        Thread.sleep(500);
+
+        getDriver().findElement(By.xpath("//label[@class='jenkins-toggle-switch__label ']")).click();
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+
+        Thread.sleep(2000);
+        getDriver().findElement(By.xpath("//span[text()='Jenkins']")).click();
+
+        WebElement disabledIcon = new WebDriverWait(getDriver(), Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(
+                        By.xpath("//td[@data='8']")
+                ));
+        Assert.assertTrue(disabledIcon.isDisplayed());
     }
 
     private void createMCProject(String name) {
