66b68d176527e199de69fbc76c42e41ba00b17e7
Владимир <izyumov.vladimir.91@mail.ru>
2025-11-03 00:36:28 +0300

    add OrganizationFolder1Test

---
+++ src/test/java/school/redrover/OrganizationFolder1Test.java
@@ -0,0 +1,41 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class OrganizationFolder1Test extends BaseTest {
+
+    @Test
+    public void testCreateOrganizationFolder() {
+
+        final String DISPLAYNAME = "Project 1";
+
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys("Organization Folder Project");
+
+        WebElement organizationFolder = getDriver()
+                .findElement(By.xpath("//li[@class='jenkins_branch_OrganizationFolder']"));
+        ((JavascriptExecutor) getDriver())
+                .executeScript("arguments[0].scrollIntoView(true);", organizationFolder);
+        organizationFolder.click();
+
+        getDriver().findElement(By.id("ok-button")).click();
+
+        getDriver().findElement(By.xpath("//input[@name='_.displayNameOrNull']"))
+                .sendKeys(DISPLAYNAME);
+
+        WebElement saveOrganizationFolder = getDriver().findElement(By.xpath("//button[@name='Submit']"));
+        ((JavascriptExecutor) getDriver())
+                .executeScript("arguments[0].scrollIntoView(true);", saveOrganizationFolder);
+        saveOrganizationFolder.click();
+
+        String resultName = getDriver().findElement(
+                By.xpath("//h1 [@class='job-index-headline page-headline']")).getText();
+
+        Assert.assertEquals(resultName, DISPLAYNAME);
+    }
+}
