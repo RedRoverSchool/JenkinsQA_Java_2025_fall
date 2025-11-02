5fe4db9d246ae65df286f600c36f3541e9ebc7f6
TinaSilver-fox <tinaisenka@gmail.com>
2025-11-02 20:50:24 +0300

    Fix-delete import and fix quote

--- src/test/java/school/redrover/NotificationApplyThemeTest.java
+++ src/test/java/school/redrover/NotificationApplyThemeTest.java
@@ -1,17 +1,12 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
-import java.time.Duration;
 
 public class NotificationApplyThemeTest extends BaseTest {
 
@@ -23,9 +18,9 @@ public class NotificationApplyThemeTest extends BaseTest {
         actions.moveToElement(profile).perform();
 
         Thread.sleep(2000);
-        getDriver().findElement(By.xpath("//*[@id=\"tippy-1\"]/div/div/div/a[4]")).click();
+        getDriver().findElement(By.xpath("//*[@id='tippy-1']/div/div/div/a[4]")).click();
         Thread.sleep(1000);
-        getDriver().findElement(By.xpath("//*[@id=\"main-panel\"]/form/div[1]/section/div[4]/div[1]/div/div[2]/div[1]/div/label")).click();
+        getDriver().findElement(By.xpath("//* [@id='main-panel']/form/div[1]/section/div[4]/div[1]/div/div[2]/div[1]/div/label")).click();
         getDriver().findElement(By.className("apply-button")).click();
         Thread.sleep(1000);
         WebElement result = getDriver().findElement(By.className("jenkins-notification--success"));
