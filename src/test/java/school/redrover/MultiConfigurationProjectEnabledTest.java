e21ea046e9bf0729ac70ba475968e7518cdd244c
alexrudak <rudakalexx@gmail.com>
2025-11-03 09:59:28 +0100

    Rename tests, make static String

--- src/test/java/school/redrover/MultiConfigurationProjectEnabledTest.java
+++ src/test/java/school/redrover/MultiConfigurationProjectEnabledTest.java
@@ -11,12 +11,13 @@ import school.redrover.common.BaseTest;
 import java.time.Duration;
 
 public class MultiConfigurationProjectEnabledTest extends BaseTest {
-    private final String projectName = "NewMulti-ConfigurationProject";
+    private static final String PROJECT_NAME = "NewMulti-ConfigurationProject";
 
     @Test
-    void disableAfterCreationTest() throws InterruptedException {
-        createMCProject(projectName);
+    void testDisableAfterCreation() throws InterruptedException {
+        createMCProject(PROJECT_NAME);
         Thread.sleep(500);
+
         getDriver().findElement(By.xpath("//label[@class='jenkins-toggle-switch__label ']")).click();
         Thread.sleep(500);
 
@@ -29,14 +30,15 @@ public class MultiConfigurationProjectEnabledTest extends BaseTest {
     }
 
     @Test
-    void disableFromHomePageTest() throws InterruptedException {
-        createMCProject(projectName);
+    void testDisableFromHomePage() throws InterruptedException {
+        createMCProject(PROJECT_NAME);
 
         WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/']"))).click();
-        getDriver().findElement(By.xpath("//a[@href='job/" + projectName + "/']")).click();
+        getDriver().findElement(By.xpath("//a[@href='job/" + PROJECT_NAME + "/']")).click();
         getDriver().findElement(By.xpath("//a[contains(@href, '/configure')]")).click();
         getDriver().findElement(By.xpath("//label[@class='jenkins-toggle-switch__label ']")).click();
+        Thread.sleep(500);
 
         Assert.assertTrue(getDriver().findElement(By.
                         xpath("//span[@class='jenkins-toggle-switch__label__unchecked-title']")).isDisplayed(),
@@ -47,13 +49,14 @@ public class MultiConfigurationProjectEnabledTest extends BaseTest {
     }
 
     @Test
-    void warningWhenDisableTest() throws InterruptedException {
-        createMCProject(projectName);
+    void testWarningWhenDisable() throws InterruptedException {
+        createMCProject(PROJECT_NAME);
         Thread.sleep(500);
 
         getDriver().findElement(By.xpath("//label[@class='jenkins-toggle-switch__label ']")).click();
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
         WebElement warning = getDriver().findElement(By.xpath("//*[@id='enable-project']"));
+        Thread.sleep(500);
 
         Assert.assertTrue(warning.getText().contains("This project is currently disabled"),
                 "Warning text should contain 'This project is currently disabled'");
@@ -66,4 +69,3 @@ public class MultiConfigurationProjectEnabledTest extends BaseTest {
         getDriver().findElement(By.id("ok-button")).click();
     }
 }
-
