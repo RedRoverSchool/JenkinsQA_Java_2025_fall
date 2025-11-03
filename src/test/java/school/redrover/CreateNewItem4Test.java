ded99af1c8b9311a1358ed46653c94fa5f3cf51b
UlianaScolnicova <yliana.tol@gmail.com>
2025-11-03 14:25:40 +0200

    itemNameInput

--- src/test/java/school/redrover/CreateNewItem4Test.java
+++ src/test/java/school/redrover/CreateNewItem4Test.java
@@ -6,6 +6,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
+import java.util.List;
+
 public class CreateNewItem4Test extends BaseTest {
 
     @Test
@@ -20,5 +22,24 @@ public class CreateNewItem4Test extends BaseTest {
 
         Assert.assertTrue(label.isDisplayed(), "Label 'Enter an item name' should be displayed");
         Assert.assertTrue(inputField.isDisplayed(), "Input field 'name' should be visible");
+    }
+
+    @Test
+    public void itemNameInput() {
+
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.xpath("//*[@id='name']")).sendKeys("Uliana_123");
+
+        List<WebElement> validationMessages = getDriver().findElements(By.className("input-validation-message"));
+
+        boolean allValidationMessagesDisabled = true;
+
+        for (WebElement message : validationMessages) {
+            if (!message.getAttribute("class").contains("input-message-disabled")) {
+                allValidationMessagesDisabled = false;
+            }
+        }
+        Assert.assertTrue(allValidationMessagesDisabled,
+                "All validation messages should be disabled for valid input");
     }
 }
