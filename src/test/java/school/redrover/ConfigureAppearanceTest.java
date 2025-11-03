65fbfe674042a59c77f83c3adb08663d0eb9a96a
Sandarella <164547843+Sandarella@users.noreply.github.com>
2025-11-03 22:39:26 +0500

    Added test for Change Theme

---
+++ src/test/java/school/redrover/ConfigureAppearanceTest.java
@@ -0,0 +1,19 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class ConfigureAppearanceTest extends BaseTest {
+    @Test
+    public void testChangeTheme() {
+
+        getDriver().findElement(By.id("root-action-ManageJenkinsAction")).click();
+        getDriver().findElement(By.cssSelector("a[href='appearance']")).click();
+        getDriver().findElement(By.cssSelector("label[for=\"radio-block-0\"]")).click();
+        getDriver().findElement(By.cssSelector("button.jenkins-submit-button")).click();
+
+        Assert.assertTrue(getDriver().findElement(By.cssSelector("html[data-theme='dark']")).isDisplayed());
+    }
+}
