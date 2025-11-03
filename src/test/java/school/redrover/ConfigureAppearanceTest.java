da279d97464cf4751bfb8959f57ef9505af76180
Sandarella <164547843+Sandarella@users.noreply.github.com>
2025-11-04 00:36:44 +0500

    RF ConfigureAppearanceTest

--- src/test/java/school/redrover/ConfigureAppearanceTest.java
+++ src/test/java/school/redrover/ConfigureAppearanceTest.java
@@ -41,4 +41,16 @@ public class ConfigureAppearanceTest extends BaseTest {
                 getDriver().findElement(By.cssSelector("html")).getAttribute("data-theme"),
                 "dark");
     }
+    @Test
+    public void testChangeThemeOld() throws InterruptedException {
+
+        getDriver().findElement(By.id("root-action-ManageJenkinsAction")).click();
+        getDriver().findElement(By.cssSelector("a[href='appearance']")).click();
+        getDriver().findElement(By.xpath("//label[contains(., 'Do not allow users to select a different theme')]")).click();
+        Thread.sleep(2000);
+        getDriver().findElement(By.cssSelector("label:has(> div[data-theme='dark'])")).click();
+        getDriver().findElement(By.cssSelector("button.jenkins-submit-button")).click();
+
+        Assert.assertTrue(getDriver().findElement(By.cssSelector("html[data-theme='dark']")).isDisplayed());
+    }
 }
