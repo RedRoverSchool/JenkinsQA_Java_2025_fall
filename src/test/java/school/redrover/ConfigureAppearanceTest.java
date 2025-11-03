5c43cb60b4ec5e22ef3da0c66e01fd598a0cfb52
Sandarella <164547843+Sandarella@users.noreply.github.com>
2025-11-04 00:36:44 +0500

    RF ConfigureAppearanceTest

--- src/test/java/school/redrover/ConfigureAppearanceTest.java
+++ src/test/java/school/redrover/ConfigureAppearanceTest.java
@@ -10,11 +10,13 @@ public class ConfigureAppearanceTest extends BaseTest {
 
     @Ignore
     @Test
-    public void testChangeTheme() {
+    public void testChangeTheme() throws InterruptedException {
 
         getDriver().findElement(By.id("root-action-ManageJenkinsAction")).click();
         getDriver().findElement(By.cssSelector("a[href='appearance']")).click();
-        getDriver().findElement(By.cssSelector("label[for=\"radio-block-0\"]")).click();
+        getDriver().findElement(By.xpath("//label[contains(., 'Do not allow users to select a different theme')]")).click();
+        Thread.sleep(2000);
+        getDriver().findElement(By.cssSelector("label:has(> div[data-theme='dark'])")).click();
         getDriver().findElement(By.cssSelector("button.jenkins-submit-button")).click();
 
         Assert.assertTrue(getDriver().findElement(By.cssSelector("html[data-theme='dark']")).isDisplayed());
