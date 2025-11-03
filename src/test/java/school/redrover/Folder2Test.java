a542e90c7e99069a15afbe542cace3ef423a41df
kiramitt <kiramitt@gmail.com>
2025-11-03 14:27:30 +0300

    AT_01.002.11 | New Item > Folder > Put item to the folder

--- src/test/java/school/redrover/Folder2Test.java
+++ src/test/java/school/redrover/Folder2Test.java
@@ -2,6 +2,8 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -15,20 +17,25 @@ import java.util.UUID;
 
 public class Folder2Test extends BaseTest {
 
-    private void createFolder(String folderName) {
+    private void createItem(String itemName, String item) {
         getDriver().findElement(By.linkText("New Item")).click();
-        getDriver().findElement(By.id("name")).sendKeys(folderName);
-        getDriver().findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder")).click();
+        getDriver().findElement(By.id("name")).sendKeys(itemName);
+        String itemClassName = switch (item) {
+            case "Folder" -> "com_cloudbees_hudson_plugins_folder_Folder";
+            case "Pipeline" -> "org_jenkinsci_plugins_workflow_job_WorkflowJob";
+            default -> "";
+        };
+        getDriver().findElement(By.className(itemClassName)).click();
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.name("Submit")).click();
         new WebDriverWait(getDriver(), Duration.ofSeconds(10)).until(driver -> Objects.requireNonNull(
-                driver.getCurrentUrl()).endsWith("/job/%s/".formatted(folderName)));
+                driver.getCurrentUrl()).endsWith("/job/%s/".formatted(itemName)));
     }
 
     @Test
     public void testCreateFolder() {
         final String folderName = "Folder" + UUID.randomUUID().toString().substring(0, 5);
-        createFolder(folderName);
+        createItem(folderName, "Folder");
 
         Assert.assertEquals(
                 getDriver().findElement(By.tagName("h1")).getText(),
@@ -46,8 +53,8 @@ public class Folder2Test extends BaseTest {
         final String parentFolderName = "Folder" + UUID.randomUUID().toString().substring(0, 5);
         final String childFolderName = "Folder" + UUID.randomUUID().toString().substring(0, 5);
 
-        createFolder(parentFolderName);
-        createFolder(childFolderName);
+        createItem(parentFolderName, "Folder");
+        createItem(childFolderName, "Folder");
 
         List<String> breadcrumbTexts = new ArrayList<>();
         for (WebElement element : getDriver().findElements(By.xpath("//ol[@id='breadcrumbs']/li/a"))) {
@@ -61,6 +68,42 @@ public class Folder2Test extends BaseTest {
         Assert.assertEquals(
                 breadcrumbTexts,
                 folderNames,
+                "Путь хлебных крошек не соответствует ожиданию");
+    }
+
+    @Test
+    public void testPutPipelineToFolder() {
+        final String folderName = "Folder" + UUID.randomUUID().toString().substring(0, 5);
+        final String pipelineName = "Pipeline" + UUID.randomUUID().toString().substring(0, 5);
+
+        createItem(folderName, "Folder");
+        getDriver().findElement(By.className("jenkins-mobile-hide")).click();
+        createItem(pipelineName, "Pipeline");
+
+        getDriver().findElement(By.className("jenkins-mobile-hide")).click();
+
+        Actions actions = new Actions(getDriver());
+        actions.moveToElement(getDriver().findElement(
+                By.xpath("//a[.//span[contains(text(), '%s')]]".formatted(pipelineName)))).perform();
+        actions.moveToElement(getDriver().findElement(By.xpath(("//button[contains(@class, 'jenkins-menu-dropdown-chevron')" +
+                " and contains(@data-href, '%s')]").formatted(pipelineName)))).click().build().perform();
+        getDriver().findElement(By.xpath("//a[contains(., 'Move')]")).click();
+
+        Select selectObject = new Select(getDriver().findElement(By.className("jenkins-select__input")));
+        selectObject.selectByVisibleText("Jenkins » %s".formatted(folderName));
+        getDriver().findElement(By.name("Submit")).click();
+
+        List<String> breadcrumbTexts = new ArrayList<>();
+        for (WebElement element : getDriver().findElements(By.xpath("//ol[@id='breadcrumbs']/li/a"))) {
+            breadcrumbTexts.add(element.getText());
+        }
+
+        List<String> itemsNames = new ArrayList<>();
+        itemsNames.add(folderName);
+        itemsNames.add(pipelineName);
+        Assert.assertEquals(
+                breadcrumbTexts,
+                itemsNames,
                 "Путь хлебных крошек не соответствует ожиданию");
     }
 }
