95a65dd706b13c555490c80acf4fa17052371338
xkaktyzx <xkaktyzx1@gmail.com>
2025-11-04 16:03:29 +0100

    moved the REST API header check to a separate method

--- src/test/java/school/redrover/FooterRestApiTest.java
+++ src/test/java/school/redrover/FooterRestApiTest.java
@@ -7,14 +7,39 @@ import school.redrover.common.BaseTest;
 
 public class FooterRestApiTest extends BaseTest {
 
-    @Test
-    public void testRestApiMainPage() {
+    private void checkRestApiHeader() {
         final String restApiText = "REST API";
-
         getDriver().findElement(By.className("rest-api")).click();
 
         String actualText = getDriver().findElement(By.cssSelector("h1")).getText();
 
         Assert.assertEquals(actualText, restApiText, "Текст заголовка не совпадает с ожидаемым");
+    }
+
+
+    @Test
+    public void testRestApiMainPage() {
+        checkRestApiHeader();
+    }
+
+    @Test
+    public void testRestApiUserAdminPage() {
+        getDriver().findElement(By.id("root-action-UserAction")).click();
+
+        checkRestApiHeader();
+    }
+
+    @Test
+    public void testRestApiBuildsPage() {
+        getDriver().findElement(By.xpath("//a[@href='/view/all/builds']")).click();
+
+        checkRestApiHeader();
+    }
+
+    @Test
+    public void testRestApiComputerPage() {
+        getDriver().findElement(By.xpath("//a[@href='computer/new']")).click();
+
+        checkRestApiHeader();
     }
 }
