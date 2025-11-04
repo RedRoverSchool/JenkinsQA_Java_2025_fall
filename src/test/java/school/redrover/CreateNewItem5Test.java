855cb3a8c931cada59a20345d1b4041b32b4c496
AnnaMelnikova <annamelpt@gmail.com>
2025-11-04 10:54:34 +0000

    Add check for projects

--- src/test/java/school/redrover/CreateNewItem5Test.java
+++ src/test/java/school/redrover/CreateNewItem5Test.java
@@ -6,21 +6,34 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class CreateNewItem5Test extends BaseTest {
 
     @Test
-    public void createNewItem () {
+    public void createNewItem() throws InterruptedException {
+        List<String> expectedTextsProjects =
+                List.of("Freestyle project", "Pipeline", "Multi-configuration project",
+                        "Folder", "Multibranch Pipeline", "Organization Folder");
+
         WebElement newItemButton = getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[1]/span/a"));
         newItemButton.click();
 
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Thread.sleep(2000);
 
         WebElement checkTextInput = getDriver().findElement(By.id("name"));
         Assert.assertTrue(checkTextInput.isDisplayed(), "The input field is not present on the page");
+
+        List<WebElement> projectsNames = getDriver().findElements(By.cssSelector(".label"));
+
+        List<String> actualProjectNames = projectsNames.stream().map(WebElement::getText).toList();
+
+        System.out.println("Actual labels: " + actualProjectNames);
+
+        Assert.assertEquals(actualProjectNames, (expectedTextsProjects));
+
     }
 }
