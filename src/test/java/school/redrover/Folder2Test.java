2281ceab0984f78e291ea005cb27bff8436cf1b0
kiramitt <kiramitt@gmail.com>
2025-11-04 14:39:02 +0300

    Edit

--- src/test/java/school/redrover/Folder2Test.java
+++ src/test/java/school/redrover/Folder2Test.java
@@ -1,10 +1,12 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
@@ -16,15 +18,12 @@ import java.util.UUID;
 
 public class Folder2Test extends BaseTest {
 
-    private void createItem(String itemName, String item) {
+    private void createItem(String itemName, String itemType) {
         getDriver().findElement(By.linkText("New Item")).click();
         getDriver().findElement(By.id("name")).sendKeys(itemName);
-        String itemClassName = switch (item) {
-            case "Folder" -> "com_cloudbees_hudson_plugins_folder_Folder";
-            case "Pipeline" -> "org_jenkinsci_plugins_workflow_job_WorkflowJob";
-            default -> "";
-        };
-        getDriver().findElement(By.className(itemClassName)).click();
+        WebElement selectedItemType = getDriver().findElement(By.xpath("//span[text()='%s']".formatted(itemType)));
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);", selectedItemType);
+        selectedItemType.click();
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.name("Submit")).click();
         new WebDriverWait(getDriver(), Duration.ofSeconds(10)).until(driver -> Objects.requireNonNull(
@@ -33,7 +32,7 @@ public class Folder2Test extends BaseTest {
 
     @Test
     public void testCreateFolder() {
-        final String folderName = "Folder" + UUID.randomUUID().toString().substring(0, 5);
+        final String folderName = "Folder" + UUID.randomUUID().toString().substring(0, 3);
         createItem(folderName, "Folder");
 
         Assert.assertEquals(
@@ -49,8 +48,8 @@ public class Folder2Test extends BaseTest {
 
     @Test
     public void testNewFolderDefaultAddedToExistingFolder() {
-        final String parentFolderName = "Folder" + UUID.randomUUID().toString().substring(0, 5);
-        final String childFolderName = "Folder" + UUID.randomUUID().toString().substring(0, 5);
+        final String parentFolderName = "Folder" + UUID.randomUUID().toString().substring(0, 3);
+        final String childFolderName = "Folder" + UUID.randomUUID().toString().substring(0, 3);
 
         createItem(parentFolderName, "Folder");
         createItem(childFolderName, "Folder");
@@ -70,20 +69,40 @@ public class Folder2Test extends BaseTest {
                 "Путь хлебных крошек не соответствует ожиданию");
     }
 
-    @Test
-    public void testPutPipelineToFolder() {
-        final String folderName = "Folder" + UUID.randomUUID().toString().substring(0, 5);
-        final String pipelineName = "Pipeline" + UUID.randomUUID().toString().substring(0, 5);
+    @DataProvider(name = "itemsProvider")
+    public Object[][] itemsProvider() {
+        String freestyleName = "Freestyle" + UUID.randomUUID().toString().substring(0, 3);
+        String pipelineName = "Pipeline" + UUID.randomUUID().toString().substring(0, 3);
+        String multiConfigPrName = "MultiConfigPr" + UUID.randomUUID().toString().substring(0, 3);
+        String folderName = "Folder" + UUID.randomUUID().toString().substring(0, 3);
+        String multibrPipName = "MultibrPip" + UUID.randomUUID().toString().substring(0, 3);
+        String orgFolderName = "OrgFolder" + UUID.randomUUID().toString().substring(0, 3);
+
+        return new Object[][]{
+                { "Freestyle project", freestyleName },
+                { "Pipeline", pipelineName },
+                { "Multi-configuration project", multiConfigPrName },
+                { "Folder", folderName },
+                { "Multibranch Pipeline", multibrPipName },
+                { "Organization Folder", orgFolderName }
+        };
+    }
+
+    @Test(dataProvider = "itemsProvider")
+    public void testPutItemToFolder(String itemType, String itemName) {
+        final String folderName = "Folder" + UUID.randomUUID().toString().substring(0, 3);
 
         createItem(folderName, "Folder");
         getDriver().findElement(By.className("jenkins-mobile-hide")).click();
-        createItem(pipelineName, "Pipeline");
+        createItem(itemName, itemType);
 
         getDriver().findElement(By.xpath("//a[contains(@href, 'move')]")).click();
         Select selectObject = new Select(getDriver().findElement(By.className("jenkins-select__input")));
         selectObject.selectByVisibleText("Jenkins » %s".formatted(folderName));
         getDriver().findElement(By.name("Submit")).click();
 
+        new WebDriverWait(getDriver(), Duration.ofSeconds(10)).until(driver -> Objects.requireNonNull(
+                driver.getCurrentUrl()).endsWith("/job/%s/".formatted(itemName)));
         List<String> breadcrumbTexts = new ArrayList<>();
         for (WebElement element : getDriver().findElements(By.xpath("//*[@id='breadcrumbs']//a"))) {
             breadcrumbTexts.add(element.getText());
@@ -91,7 +110,7 @@ public class Folder2Test extends BaseTest {
 
         List<String> itemsNames = new ArrayList<>();
         itemsNames.add(folderName);
-        itemsNames.add(pipelineName);
+        itemsNames.add(itemName);
         Assert.assertEquals(
                 breadcrumbTexts,
                 itemsNames,
