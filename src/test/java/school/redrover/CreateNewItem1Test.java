cf08864d16475daf564f9c90109bf4dfb2a51b82
Alexey Vorobyev <118996115+AlexeiVorobyev@users.noreply.github.com>
2025-11-05 14:28:59 -0800

    US_02.006 | Freestyle Project Configuration > Build Steps (#754)
    * US_02.006 | Freestyle Project Configuration > Build Steps
    
    * US_02.006 | Freestyle Project Configuration > Build Steps

--- src/test/java/school/redrover/CreateNewItem1Test.java
+++ src/test/java/school/redrover/CreateNewItem1Test.java
@@ -2,9 +2,14 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.openqa.selenium.JavascriptExecutor;
 import school.redrover.common.BaseTest;
+
+import java.time.Duration;
 
 public class CreateNewItem1Test extends BaseTest {
 
@@ -47,6 +52,58 @@ public class CreateNewItem1Test extends BaseTest {
 
             Assert.assertEquals(dataValid, "false",
                     "Character '" + ch + "' should not be allowed");
+        }
+    }
+
+    @Test
+    public void testConfigurationPageIsVisible() {
+
+        getDriver().findElement(By.xpath("//span[text()='Create a job']")).click();
+
+        getDriver().findElement(By.id("name")).sendKeys("TestProject");
+        getDriver().findElement(By.xpath("//span[text()='Freestyle project']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//span[text() = 'Configuration']")).getText(),"Configuration");
+    }
+
+    @Test
+    public void testFreestyleProjectConfigBuildSteps() {
+
+        String[] expectedBuildSteps = {
+                "Execute Windows batch command",
+                "Execute shell",
+                "Invoke Ant",
+                "Invoke Gradle script",
+                "Invoke top-level Maven targets",
+                "Run with timeout",
+                "Set build status to \"pending\" on GitHub commit"
+        };
+
+        getDriver().findElement(By.xpath("//span[text()='Create a job']")).click();
+
+        getDriver().findElement(By.id("name")).sendKeys("TestProject");
+        getDriver().findElement(By.xpath("//span[text()='Freestyle project']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+
+        WebElement addBuildStep = new WebDriverWait(getDriver(), Duration.ofSeconds(5))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(text(),'Add build')]")));
+
+        ((JavascriptExecutor) getDriver())
+                .executeScript("arguments[0].scrollIntoView({block: 'center'});", addBuildStep);
+
+        addBuildStep.click();
+
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//button[normalize-space()='Execute Windows batch command']")
+        ));
+
+
+        for (int i = 0; i < expectedBuildSteps.length; i++) {
+            WebElement actualBuildStep = getDriver().findElement(By.xpath("//button[contains(text(),'%s')]".formatted(expectedBuildSteps[i])));
+
+            Assert.assertEquals(actualBuildStep.getText(),expectedBuildSteps[i]);
         }
     }
 }
