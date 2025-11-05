98f74212891cb06a6bd1c68f4796b5826dd98efe
kiramitt <kiramitt@gmail.com>
2025-11-05 17:30:21 +0300

    AT_01.002.17 | New Item > Folder > Find items in folder

--- src/test/java/school/redrover/Folder2Test.java
+++ src/test/java/school/redrover/Folder2Test.java
@@ -3,6 +3,7 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -13,6 +14,7 @@ import school.redrover.common.BaseTest;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
@@ -31,6 +33,14 @@ public class Folder2Test extends BaseTest {
                 driver.getCurrentUrl()).endsWith("/job/%s/".formatted(itemName)));
     }
 
+    private List<String> getTextsOfItems(String xpathLocator) {
+        List<String> itemsTexts = new ArrayList<>();
+        for (WebElement element : getDriver().findElements(By.xpath(xpathLocator))) {
+            itemsTexts.add(element.getText());
+        }
+        return itemsTexts;
+    }
+
     @Test
     public void testCreateFolder() {
         final String folderName = "Folder" + UUID.randomUUID().toString().substring(0, 3);
@@ -43,8 +53,9 @@ public class Folder2Test extends BaseTest {
         Assert.assertTrue(
                 getDriver().findElement(By.className("empty-state-section")).getText().contains("This folder is empty"),
                 "Отсутствует сообщение 'This folder is empty'");
-        List<WebElement> itemsInFolder = getDriver().findElements(By.xpath("//*[@id='projectstatus']/tbody/tr"));
-        Assert.assertTrue(itemsInFolder.isEmpty(), "Элементы должны отсутствовать в новой таблице");
+
+        Assert.assertTrue(getTextsOfItems("//*[contains(@class, 'jenkins-table__link')]").isEmpty(),
+                "Элементы должны отсутствовать в новой таблице");
     }
 
     @Test
@@ -55,18 +66,9 @@ public class Folder2Test extends BaseTest {
         createItem(parentFolderName, "Folder");
         createItem(childFolderName, "Folder");
 
-        List<String> breadcrumbTexts = new ArrayList<>();
-        for (WebElement element : getDriver().findElements(By.xpath("//ol[@id='breadcrumbs']/li/a"))) {
-            breadcrumbTexts.add(element.getText());
-        }
-
-        List<String> folderNames = new ArrayList<>();
-        folderNames.add(parentFolderName);
-        folderNames.add(childFolderName);
-
         Assert.assertEquals(
-                breadcrumbTexts,
-                folderNames,
+                getTextsOfItems("//ol[@id='breadcrumbs']/li/a"),
+                List.of(parentFolderName, childFolderName),
                 "Путь хлебных крошек не соответствует ожиданию");
     }
 
@@ -104,17 +106,10 @@ public class Folder2Test extends BaseTest {
 
         new WebDriverWait(getDriver(), Duration.ofSeconds(10)).until(driver -> Objects.requireNonNull(
                 driver.getCurrentUrl()).endsWith("/job/%s/".formatted(itemName)));
-        List<String> breadcrumbTexts = new ArrayList<>();
-        for (WebElement element : getDriver().findElements(By.xpath("//*[@id='breadcrumbs']//a"))) {
-            breadcrumbTexts.add(element.getText());
-        }
 
-        List<String> itemsNames = new ArrayList<>();
-        itemsNames.add(folderName);
-        itemsNames.add(itemName);
         Assert.assertEquals(
-                breadcrumbTexts,
-                itemsNames,
+                getTextsOfItems("//ol[@id='breadcrumbs']/li/a"),
+                List.of(folderName, itemName),
                 "Путь хлебных крошек не соответствует ожиданию");
     }
 
@@ -164,21 +159,56 @@ public class Folder2Test extends BaseTest {
         getDriver().findElement(By.className("jenkins-mobile-hide")).click();
 
         getDriver().findElement(By.xpath("//span[text()='%s']".formatted(folder1Name))).click();
-        List<String> folder1Items = new ArrayList<>();
-        for (WebElement element : getDriver().findElements(By.className("jenkins-table__link"))) {
-            folder1Items.add(element.getText());
-        }
-
+        List<String> folder1Items = getTextsOfItems("//*[contains(@class, 'jenkins-table__link')]");
         getDriver().findElement(By.className("jenkins-mobile-hide")).click();
         getDriver().findElement(By.xpath("//span[text()='%s']".formatted(folder2Name))).click();
-        List<String> folder2Items = new ArrayList<>();
-        for (WebElement element : getDriver().findElements(By.className("jenkins-table__link"))) {
-            folder2Items.add(element.getText());
-        }
+        List<String> folder2Items = getTextsOfItems("//*[contains(@class, 'jenkins-table__link')]");
 
         Assert.assertEquals(
                 folder1Items,
                 folder2Items,
                 "Несоответствие содержимого папок");
+    }
+
+    @Test
+    public void testFindFolderContents() {
+        final String folderName = "Folder" + UUID.randomUUID().toString().substring(0, 3);
+        final String pipelineName = "Pipeline" + UUID.randomUUID().toString().substring(0, 3);
+        final String freestyleName = "Freestyle" + UUID.randomUUID().toString().substring(0, 3);
+
+        createItem(folderName, "Folder");
+        createItem(pipelineName, "Pipeline");
+        getDriver().findElement(By.xpath("//a[text()='%s']".formatted(folderName))).click();
+        createItem(freestyleName, "Freestyle project");
+        getDriver().findElement(By.xpath("//a[text()='%s']".formatted(folderName))).click();
+
+        Assert.assertEquals(
+                new HashSet<>(getTextsOfItems("//*[contains(@class, 'jenkins-table__link')]")),
+                new HashSet<>(List.of(freestyleName, pipelineName)),
+                "Неверное отображение элементов");
+
+        getDriver().findElement(By.className("jenkins-mobile-hide")).click();
+        getDriver().findElement(By.id("root-action-SearchAction")).click();
+        WebElement searchInput = getDriver().findElement(By.id("command-bar"));
+        searchInput.sendKeys(pipelineName);
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(
+                "//*[contains(text(), 'Get help using Jenkins search')]")));
+
+        Assert.assertTrue(getTextsOfItems("//*[@id='search-results']//a").
+                        contains("%s » %s".formatted(folderName, pipelineName)),
+                "Список результатов поиска не содержит нужный элемент");
+
+        new Actions(getDriver())
+                .moveToElement(searchInput, 0, -50)
+                .click()
+                .perform();
+        getDriver().findElement(By.id("root-action-SearchAction")).click();
+        getDriver().findElement(By.id("command-bar")).sendKeys(freestyleName);
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(
+                "//*[contains(text(), 'Get help using Jenkins search')]")));
+        Assert.assertTrue(getTextsOfItems("//*[@id='search-results']//a").
+                        contains("%s » %s".formatted(folderName, freestyleName)),
+                "Список результатов поиска не содержит нужный элемент");
     }
 }
