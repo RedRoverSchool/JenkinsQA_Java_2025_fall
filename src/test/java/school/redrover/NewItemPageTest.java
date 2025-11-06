610513937bc8eebef2d0d28f2079257503e3bf73
Denis Lafa <denislafa@gmail.com>
2025-11-06 17:53:44 +0300

    testSelectItemTypeIsVisible

--- src/test/java/school/redrover/NewItemPageTest.java
+++ src/test/java/school/redrover/NewItemPageTest.java
@@ -61,4 +61,15 @@ public class NewItemPageTest extends BaseTest {
         Assert.assertEquals(getDriver().findElement(By.cssSelector("#projectstatus > :nth-child(2) > :nth-child(1) > :nth-child(3) > a > span")).getText(), itemName);
 
     }
+
+    @Test
+    public void testSelectItemTypeIsVisible() {
+
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+        WebElement sectionTitle = getDriver().findElement(By.xpath("//div[text()='Select an item type']"));
+        WebElement itemsList = getDriver().findElement(By.id("items"));
+
+        Assert.assertTrue(sectionTitle.isDisplayed());
+        Assert.assertTrue(itemsList.isDisplayed());
+    }
 }
