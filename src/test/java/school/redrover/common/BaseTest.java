3b184bcbf3b007bdd4d4eb51b296fa72dc26cdc5
Sergey.D <sergei.demyanenko@gmail.com>
2025-11-06 23:06:24 -0500

    Add wait and remove "old" folder

--- src/test/java/school/redrover/common/BaseTest.java
+++ src/test/java/school/redrover/common/BaseTest.java
@@ -1,14 +1,20 @@
 package school.redrover.common;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 
 import java.lang.reflect.Method;
+import java.time.Duration;
 
 public abstract class BaseTest {
 
     private WebDriver driver;
+
+    private WebDriverWait wait2;
+    private WebDriverWait wait5;
+    private WebDriverWait wait10;
 
     @BeforeMethod
     protected void beforeMethod(Method method) {
@@ -32,6 +38,9 @@ public abstract class BaseTest {
             } catch (Exception ignore) {}
 
             driver.quit();
+            wait2 = null;
+            wait5 = null;
+            wait10 = null;
         }
 
         ProjectUtils.logf("Execution time is %.3f sec", (testResult.getEndMillis() - testResult.getStartMillis()) / 1000.0);
@@ -39,5 +48,29 @@ public abstract class BaseTest {
 
     protected WebDriver getDriver() {
         return driver;
+    }
+
+    protected WebDriverWait getWait2() {
+        if (wait2 == null) {
+            wait2 = new WebDriverWait(getDriver(), Duration.ofSeconds(2));
+        }
+
+        return wait2;
+    }
+
+    protected WebDriverWait getWait5() {
+        if (wait5 == null) {
+            wait5 = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        }
+
+        return wait5;
+    }
+
+    protected WebDriverWait getWait10() {
+        if (wait10 == null) {
+            wait10 = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        }
+
+        return wait10;
     }
 }
