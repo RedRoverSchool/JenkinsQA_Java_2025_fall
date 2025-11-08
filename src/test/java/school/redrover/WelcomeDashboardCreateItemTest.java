bd4b23a3c4bc2f1e4513f07c46461d6ecc9c153c
Vlas Mastykash <mastykash.vlas@gmail.com>
2025-11-09 03:24:24 +0600

    #906: RF_11.002.03 | CreateJobTest > testSuccessfulFreeStyleJobCreation()

--- src/test/java/school/redrover/WelcomeDashboardCreateItemTest.java
+++ src/test/java/school/redrover/WelcomeDashboardCreateItemTest.java
@@ -3,40 +3,37 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
+import java.util.List;
+import java.util.Map;
+
 public class WelcomeDashboardCreateItemTest extends BaseTest {
 
     @DataProvider(name = "newItemLocators")
     public Object[][] newItemLocators() {
-        final By newItemLink = By.linkText("New Item");
-        final By createJobLink = By.linkText("Create a job");
+        List<By> links = List.of(
+                By.linkText("New Item"),
+                By.linkText("Create a job")
+        );
 
-        final By freestyleProjectItem = By.className("hudson_model_FreeStyleProject");
-        final By pipelineItem = By.className("org_jenkinsci_plugins_workflow_job_WorkflowJob");
-        final By multiConfigurationProjectItem = By.className("hudson_matrix_MatrixProject");
-        final By folderItem = By.className("com_cloudbees_hudson_plugins_folder_Folder");
-        final By multibranchPipelineItem = By.className("org_jenkinsci_plugins_workflow_multibranch_WorkflowMultiBranchProject");
-        final By organizationFolderItem = By.className("jenkins_branch_OrganizationFolder");
+        Map<By, String> items = Map.of(
+                By.className("hudson_model_FreeStyleProject"), "Test - Freestyle Project",
+                By.className("org_jenkinsci_plugins_workflow_job_WorkflowJob"), "Test - Pipeline",
+                By.className("hudson_matrix_MatrixProject"), "Test - Multi-configuration Project",
+                By.className("com_cloudbees_hudson_plugins_folder_Folder"), "Test - Folder",
+                By.className("org_jenkinsci_plugins_workflow_multibranch_WorkflowMultiBranchProject"), "Test - Multibranch Pipeline",
+                By.className("jenkins_branch_OrganizationFolder"), "Test - Organization Folder"
+        );
 
-        return new Object[][]{
-                {newItemLink, freestyleProjectItem, "Test Freestyle Project"},
-                {newItemLink, pipelineItem, "Test Pipeline"},
-                {newItemLink, multiConfigurationProjectItem, "Test Multi-configuration Project"},
-                {newItemLink, folderItem, "Test Folder"},
-                {newItemLink, multibranchPipelineItem, "Test Multibranch Pipeline"},
-                {newItemLink, organizationFolderItem, "Test Organization Folder"},
-
-                {createJobLink, freestyleProjectItem, "Test Freestyle Project"},
-                {createJobLink, pipelineItem, "Test Pipeline"},
-                {createJobLink, multiConfigurationProjectItem, "Test Multi-configuration Project"},
-                {createJobLink, folderItem, "Test Folder"},
-                {createJobLink, multibranchPipelineItem, "Test Multibranch Pipeline"},
-                {createJobLink, organizationFolderItem, "Test Organization Folder"},
-        };
+        return links.stream()
+                .flatMap(link -> items.entrySet().stream()
+                        .map(entry -> new Object[]{link, entry.getKey(), entry.getValue()}))
+                .toArray(Object[][]::new);
     }
 
     @Test(dataProvider = "newItemLocators")
@@ -50,7 +47,8 @@ public class WelcomeDashboardCreateItemTest extends BaseTest {
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.name("Submit")).click();
 
-        WebElement createdItemHeader = getDriver().findElement(By.className("page-headline"));
+        WebElement createdItemHeader = getWait5().until(ExpectedConditions.visibilityOfElementLocated(By
+                .className("page-headline")));
         Assert.assertEquals(createdItemHeader.getText(), itemName);
     }
 
