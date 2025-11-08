eff07201027627d660fee11e2f334f8d9f413770
Vlas Mastykash <mastykash.vlas@gmail.com>
2025-11-09 01:56:01 +0600

    #906: RF_11.002.03 | CreateJobTest > testSuccessfulFreeStyleJobCreation()

--- src/test/java/school/redrover/WelcomeDashboardCreateItemTest.java
+++ src/test/java/school/redrover/WelcomeDashboardCreateItemTest.java
@@ -3,6 +3,7 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -12,11 +13,8 @@ public class WelcomeDashboardCreateItemTest extends BaseTest {
 
     @DataProvider(name = "newItemLocators")
     public Object[][] newItemLocators() {
-        final String newItemText = "New Item";
-        final String createJobText = "Create a job";
-
-        final By newItemLink = By.linkText(newItemText);
-        final By createJobLink = By.linkText(createJobText);
+        final By newItemLink = By.linkText("New Item");
+        final By createJobLink = By.linkText("Create a job");
 
         final By freestyleProjectItem = By.className("hudson_model_FreeStyleProject");
         final By pipelineItem = By.className("org_jenkinsci_plugins_workflow_job_WorkflowJob");
@@ -26,24 +24,24 @@ public class WelcomeDashboardCreateItemTest extends BaseTest {
         final By organizationFolderItem = By.className("jenkins_branch_OrganizationFolder");
 
         return new Object[][]{
-                {newItemLink, newItemText, freestyleProjectItem, "Test Freestyle Project"},
-                {newItemLink, newItemText, pipelineItem, "Test Pipeline"},
-                {newItemLink, newItemText, multiConfigurationProjectItem, "Test Multi-configuration Project"},
-                {newItemLink, newItemText, folderItem, "Test Folder"},
-                {newItemLink, newItemText, multibranchPipelineItem, "Test Multibranch Pipeline"},
-                {newItemLink, newItemText, organizationFolderItem, "Test Organization Folder"},
+                {newItemLink, freestyleProjectItem, "Test Freestyle Project"},
+                {newItemLink, pipelineItem, "Test Pipeline"},
+                {newItemLink, multiConfigurationProjectItem, "Test Multi-configuration Project"},
+                {newItemLink, folderItem, "Test Folder"},
+                {newItemLink, multibranchPipelineItem, "Test Multibranch Pipeline"},
+                {newItemLink, organizationFolderItem, "Test Organization Folder"},
 
-                {createJobLink, createJobText, freestyleProjectItem, "Test Freestyle Project"},
-                {createJobLink, createJobText, pipelineItem, "Test Pipeline"},
-                {createJobLink, createJobText, multiConfigurationProjectItem, "Test Multi-configuration Project"},
-                {createJobLink, createJobText, folderItem, "Test Folder"},
-                {createJobLink, createJobText, multibranchPipelineItem, "Test Multibranch Pipeline"},
-                {createJobLink, createJobText, organizationFolderItem, "Test Organization Folder"},
+                {createJobLink, freestyleProjectItem, "Test Freestyle Project"},
+                {createJobLink, pipelineItem, "Test Pipeline"},
+                {createJobLink, multiConfigurationProjectItem, "Test Multi-configuration Project"},
+                {createJobLink, folderItem, "Test Folder"},
+                {createJobLink, multibranchPipelineItem, "Test Multibranch Pipeline"},
+                {createJobLink, organizationFolderItem, "Test Organization Folder"},
         };
     }
 
     @Test(dataProvider = "newItemLocators")
-    public void testSuccessfulItemCreation(By linkLocator, String linkText, By itemLocator, String itemName) {
+    public void testSuccessfulItemCreation(By linkLocator, By itemLocator, String itemName) {
         getDriver().findElement(linkLocator).click();
 
         getDriver().findElement(By.id("name")).sendKeys(itemName);
@@ -53,11 +51,9 @@ public class WelcomeDashboardCreateItemTest extends BaseTest {
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.name("Submit")).click();
 
-        WebElement createdItemHeader = getDriver().findElement(By.className("page-headline"));
-        Assert.assertEquals(
-                createdItemHeader.getText(),
-                itemName,
-                "Start link: <%s>, Test for item: <%s>".formatted(linkText, itemName));
+        WebElement createdItemHeader = getWait2().until(ExpectedConditions.visibilityOfElementLocated(By
+                .className("page-headline")));
+        Assert.assertEquals(createdItemHeader.getText(), itemName);
     }
 
     public void scrollToElement(WebElement element) {
