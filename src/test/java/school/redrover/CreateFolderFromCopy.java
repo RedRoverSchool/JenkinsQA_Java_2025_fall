5ccc6b20da59c7b368bc72ed3a3f0eccf122c75a
gaitanchristina92 <gaitanchristina@gmail.com>
2025-11-08 11:33:12 +0300

    ID: RF_01.003.02 | CreateFolderFromCopy > createCopy Moved the createFolder() method to a separate private method below the actual test (to make the class more functional for further testing. Also improved the Asserts by adding a check that the actual field exists (not only the "copy from" text).

--- src/test/java/school/redrover/CreateFolderFromCopy.java
+++ src/test/java/school/redrover/CreateFolderFromCopy.java
@@ -9,31 +9,50 @@ import school.redrover.common.BaseTest;
 
 public class CreateFolderFromCopy extends BaseTest {
 
-    @Test
-    public void createFolderAndCopyItem() {
+    final String folderName = "MyFirstFolder";
+    final String displayName = "Name for Autotest attempt";
+    final String description = "Description for Autotest attempt";
 
-        //First create the folder
+    @Test
+    public void createCopy() {
+
+        createFolder();
+        boolean fieldExists = true;
+
+        WebElement newItemButton = getDriver().
+                findElement(By.xpath("//a[@href='/job/MyFirstFolder/newJob']"));
+        newItemButton.click();
+        ((JavascriptExecutor) getDriver()).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+
+        try {
+            getDriver().findElement(By.id("from")).sendKeys(folderName);
+        } catch (Exception e) {
+            fieldExists = false;
+        }
+
+        Assert.assertTrue(fieldExists);
+        Assert.assertEquals(
+                getDriver().findElement(By.className("add-item-copy")).getText(), "Copy from");
+    }
+
+    private void createFolder() {
 
         getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys("MyFirstFolder");
+        getDriver().findElement(By.id("name")).sendKeys(folderName);
         getDriver().findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder")).click();
 
         WebElement okButton = getDriver().findElement(By.id("ok-button"));
         ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);", okButton);
         okButton.click();
 
-        getDriver().findElement(By.xpath("//div[@class='setting-main']//input")).sendKeys("Name for Autotest attempt");
-        getDriver().findElement(By.xpath("//div[@class='setting-main']//textarea")).sendKeys("Description for Autotest attempt");
+        getDriver()
+                .findElement(By.xpath("//div[@class='setting-main']//input"))
+                .sendKeys(displayName);
+        getDriver()
+                .findElement(By.xpath("//div[@class='setting-main']//textarea"))
+                .sendKeys(description);
         WebElement submitButton = getDriver().findElement(By.className("jenkins-submit-button"));
         submitButton.click();
-
-        //Create a copy of the existing folder
-
-        WebElement newItemButton = getDriver().findElement(By.xpath("//a[@href='/job/MyFirstFolder/newJob']"));
-        newItemButton.click();
-
-        ((JavascriptExecutor) getDriver()).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-       Assert.assertEquals(getDriver().findElement(By.className("add-item-copy")).getText(), "Copy from");
-
     }
 }
+
