c8cb79d5fa115222f28d5383bc2cfd38bb47403c
Владимир <izyumov.vladimir.91@mail.ru>
2025-11-09 23:53:39 +0300

    US_18.008: add FreestyleProjectDeleteTest

--- src/test/java/school/redrover/FreestyleProjectDeleteTest.java
+++ src/test/java/school/redrover/FreestyleProjectDeleteTest.java
@@ -16,7 +16,7 @@ public class FreestyleProjectDeleteTest extends BaseTest {
         final String expectedText = "Welcome to Jenkins!";
         createFreestyleProject();
 
-        getDriver().findElement(By.xpath("//img[@src='/static/27515452/images/svgs/logo.svg']")).click();
+        getDriver().findElement(By.cssSelector("span.jenkins-mobile-hide")).click();
 
         getActions().moveToElement(getDriver().findElement(
                 By.xpath("//a[@class='jenkins-table__link model-link inside']")))
