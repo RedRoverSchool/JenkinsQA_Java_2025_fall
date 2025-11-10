e95e2f73d901e976e37c94abd82493c74847ef12
Евгения <73468778+kiramitt@users.noreply.github.com>
2025-11-10 20:05:44 +0300

    RF-01.002.03.04 | New Item > Folder > Create Folder > Rename Variable (#1028)
    * RF-01.002.03.04 | New Item > Folder > Create Folder > Rename Variable

--- src/test/java/school/redrover/Folder2Test.java
+++ src/test/java/school/redrover/Folder2Test.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 
 public class Folder2Test extends BaseTest {
 
-    private static final String FOLDER_NAME_1 = "Folder1";
+    private static final String MAIN_FOLDER_NAME = "MainFolder";
 
     private void createItem(String itemName, String itemType) {
         getDriver().findElement(By.linkText("New Item")).click();
@@ -29,8 +29,8 @@ public class Folder2Test extends BaseTest {
         ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);", selectedItemType);
         selectedItemType.click();
         getDriver().findElement(By.id("ok-button")).click();
-        getWait10().until(ExpectedConditions.elementToBeClickable(By.name("Submit"))).click();
-        getWait10().until(driver -> Objects.requireNonNull(driver.getCurrentUrl()).endsWith("/job/%s/".formatted(itemName)));
+        getWait5().until(ExpectedConditions.elementToBeClickable(By.name("Submit"))).click();
+        getWait5().until(driver -> Objects.requireNonNull(driver.getCurrentUrl()).endsWith("/job/%s/".formatted(itemName)));
     }
 
     private List<String> getTextsOfItems(String xpathLocator) {
@@ -43,11 +43,11 @@ public class Folder2Test extends BaseTest {
 
     @Test
     public void testCreateFolder() {
-        createItem(FOLDER_NAME_1, "Folder");
+        createItem(MAIN_FOLDER_NAME, "Folder");
 
         Assert.assertEquals(
                 getDriver().findElement(By.tagName("h1")).getText(),
-                FOLDER_NAME_1,
+                MAIN_FOLDER_NAME,
                 "Неверное название папки");
         Assert.assertTrue(
                 getDriver().findElement(By.className("empty-state-section")).getText().contains("This folder is empty"),
@@ -55,15 +55,15 @@ public class Folder2Test extends BaseTest {
     }
 
     @Test(dependsOnMethods = {"testCreateFolder"})
-    public void testNewFolderDefaultAddedToExistingFolder() throws InterruptedException {
+    public void testNewFolderDefaultAddedToExistingFolder() {
         final String childFolderName = "Folder" + UUID.randomUUID().toString().substring(0, 3);
 
-        TestUtils.clickJS(getDriver(), By.xpath("//td/a[@href='job/%s/']".formatted(FOLDER_NAME_1)));
+        TestUtils.clickJS(getDriver(), By.xpath("//td/a[@href='job/%s/']".formatted(MAIN_FOLDER_NAME)));
         createItem(childFolderName, "Folder");
 
         Assert.assertEquals(
                 getTextsOfItems("//ol[@id='breadcrumbs']/li/a"),
-                List.of(FOLDER_NAME_1, childFolderName),
+                List.of(MAIN_FOLDER_NAME, childFolderName),
                 "Путь хлебных крошек не соответствует ожиданию");
     }
 
