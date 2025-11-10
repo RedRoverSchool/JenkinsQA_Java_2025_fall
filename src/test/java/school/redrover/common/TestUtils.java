312ffcebd1f0db50af0952ca7862adc983777216
Sergey.D <sergei.demyanenko@gmail.com>
2025-11-09 22:31:49 -0500

    Add depends on test

---
+++ src/test/java/school/redrover/common/TestUtils.java
@@ -0,0 +1,18 @@
+package school.redrover.common;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class TestUtils {
+
+    public static void clickJS(WebDriver driver, WebElement element) {
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click();", element);
+    }
+
+    public static void clickJS(WebDriver driver, By locator) {
+        clickJS(driver, driver.findElement(locator));
+    }
+}
