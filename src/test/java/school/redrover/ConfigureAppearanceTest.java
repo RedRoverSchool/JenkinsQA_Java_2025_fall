f6cb16fe6b27b87201c5e1d2132c558f513855d5
kirillbokhenek <kirill.bohenek@gmail.com>
2025-11-10 21:10:30 +0200

    sleep to getWait final

--- src/test/java/school/redrover/ConfigureAppearanceTest.java
+++ src/test/java/school/redrover/ConfigureAppearanceTest.java
@@ -43,12 +43,11 @@ public class ConfigureAppearanceTest extends BaseTest {
     }
 
     @Test
-    public void testChangeThemeOld() throws InterruptedException {
+    public void testChangeThemeOld() {
 
         getDriver().findElement(By.id("root-action-ManageJenkinsAction")).click();
         getDriver().findElement(By.cssSelector("a[href='appearance']")).click();
-        getDriver().findElement(By.xpath("//label[contains(., 'Do not allow users to select a different theme')]")).click();
-        Thread.sleep(2000);
+
         getWait2().until(ExpectedConditions.elementToBeClickable(By.cssSelector("label:has(> div[data-theme='dark'])")));
 
         getDriver().findElement(By.cssSelector("label:has(> div[data-theme='dark'])")).click();
