7e83d6b8d542381cb57360bc22097ed94f57c680
kirillbokhenek <kirill.bohenek@gmail.com>
2025-11-10 20:53:29 +0200

    sleep to getWait new3

--- src/test/java/school/redrover/ConfigureAppearanceTest.java
+++ src/test/java/school/redrover/ConfigureAppearanceTest.java
@@ -47,7 +47,7 @@ public class ConfigureAppearanceTest extends BaseTest {
 
         getDriver().findElement(By.id("root-action-ManageJenkinsAction")).click();
         getDriver().findElement(By.cssSelector("a[href='appearance']")).click();
-
+        getDriver().findElement(By.xpath("//label[contains(., 'Do not allow users to select a different theme')]")).click();
         getWait10().until(ExpectedConditions.elementToBeClickable(By.cssSelector("label:has(> div[data-theme='dark'])")));
 
         getDriver().findElement(By.cssSelector("label:has(> div[data-theme='dark'])")).click();
