a17fe3c1829a042d9349ce2dddb943e4f96438bc
SergeyVyshkvarok <130347494+SergeyVyshkvarok@users.noreply.github.com>
2025-11-10 23:39:53 +0900

    RF_02.005.04 | Freestyle Project Configuration > Set up Environment > Verify the access to configuration page after refresh (#1015)
    * refactoring testSetUpEnvironmentAfterRefresh

--- src/test/java/school/redrover/ConfigurationMatrix2Test.java
+++ src/test/java/school/redrover/ConfigurationMatrix2Test.java
@@ -2,32 +2,26 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
-import java.time.Duration;
-
 public class ConfigurationMatrix2Test extends BaseTest {
-    @Test (testName = "TC_02.005.04 | Freestyle Project Configuration " +
-            "> Set up Environment " +
-            "> Verify the access to configuration page after refresh")
-    public void testSetUpEnvironmentAfterRefresh() {
 
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
-        String expectedUrl =  "http://localhost:8080/job/NewFreestyleProject/configure";
+    @Test
+    public void testSetUpEnvironmentAfterRefresh() {
 
         getDriver().findElement(By.linkText("New Item")).click();
         getDriver().findElement(By.id("name")).sendKeys("NewFreestyleProject");
         getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.xpath("//button[@data-section-id='environment']")).click();
+
+        String urlBeforeRefresh = getDriver().getCurrentUrl();
         getDriver().navigate().refresh();
-
         String urlAfterRefresh = getDriver().getCurrentUrl();
+        getWait10().until(ExpectedConditions.urlToBe(urlBeforeRefresh));
 
-        wait.until(ExpectedConditions.urlToBe(expectedUrl));
-        Assert.assertEquals(urlAfterRefresh, expectedUrl);
+        Assert.assertEquals(urlAfterRefresh, urlBeforeRefresh);
     }
 }
