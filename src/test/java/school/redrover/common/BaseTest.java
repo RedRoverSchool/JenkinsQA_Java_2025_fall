ee3c134c9101588e7e25ade923c74c7227a1170e
Sergey.D <sergei.demyanenko@gmail.com>
2025-11-09 19:56:15 -0500

    Add test order

--- src/test/java/school/redrover/common/BaseTest.java
+++ src/test/java/school/redrover/common/BaseTest.java
@@ -4,11 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
+import school.redrover.common.order.OrderForTests;
+import school.redrover.common.order.OrderUtils;
 
 import java.lang.reflect.Method;
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
-@Listeners(FilterForTests.class)
+@Listeners({FilterForTests.class, OrderForTests.class})
 public abstract class BaseTest {
 
     private WebDriver driver;
@@ -17,31 +21,84 @@ public abstract class BaseTest {
     private WebDriverWait wait5;
     private WebDriverWait wait10;
 
-    @BeforeMethod
-    protected void beforeMethod(Method method) {
-        ProjectUtils.logf("Run %s.%s", this.getClass().getName(), method.getName());
+    private OrderUtils.MethodsOrder<Method> methodsOrder;
 
+    private void startDriver() {
+        ProjectUtils.log("Browser open");
+        driver = ProjectUtils.createDriver();
+    }
+
+    private void clearData() {
         ProjectUtils.log("Clear data");
         JenkinsUtils.clearData();
-        ProjectUtils.log("Open the browser");
-        driver = ProjectUtils.createDriver();
-        ProjectUtils.log("Get the web page");
-        ProjectUtils.get(getDriver());
+    }
+
+    private void loginWeb() {
         ProjectUtils.log("Login");
         JenkinsUtils.login(getDriver());
     }
 
-    @AfterMethod
-    protected void afterMethod(Method method, ITestResult testResult) {
-        if (ProjectUtils.isRunCI() || testResult.isSuccess() || ProjectUtils.closeIfError()) {
-            try {
-                JenkinsUtils.logout(getDriver());
-            } catch (Exception ignore) {}
+    private void getWeb() {
+        ProjectUtils.log("Get web page");
+        ProjectUtils.get(getDriver());
+    }
 
+    private void stopDriver() {
+        try {
+            JenkinsUtils.logout(getDriver());
+        } catch (Exception ignore) {
+        }
+
+        closeDriver();
+    }
+
+    private void closeDriver() {
+        if (driver != null) {
             driver.quit();
+
+            driver = null;
             wait2 = null;
             wait5 = null;
             wait10 = null;
+
+            ProjectUtils.log("Browser closed");
+        }
+    }
+
+    @BeforeClass
+    protected void beforeClass() {
+        methodsOrder = OrderUtils.createMethodsOrder(
+                Arrays.stream(this.getClass().getMethods())
+                        .filter(m -> m.getAnnotation(Test.class) != null && m.getAnnotation(Ignore.class) == null)
+                        .collect(Collectors.toList()),
+                m -> m.getName(),
+                m -> m.getAnnotation(Test.class).dependsOnMethods());
+    }
+
+    @BeforeMethod
+    protected void beforeMethod(Method method) {
+        ProjectUtils.logf("Run %s.%s", this.getClass().getName(), method.getName());
+        try {
+            if (!methodsOrder.isGroupStarted(method) || methodsOrder.isGroupFinished(method)) {
+                clearData();
+                startDriver();
+                getWeb();
+                loginWeb();
+            } else {
+                getWeb();
+            }
+        } catch (Exception e) {
+            closeDriver();
+            throw e;
+        } finally {
+            methodsOrder.markAsInvoked(method);
+        }
+    }
+
+    @AfterMethod
+    protected void afterMethod(Method method, ITestResult testResult) {
+        if (methodsOrder.isGroupFinished(method) && (ProjectUtils.isRunCI() || testResult.isSuccess() || ProjectUtils.closeIfError())) {
+            stopDriver();
         }
 
         ProjectUtils.logf("Execution time is %.3f sec", (testResult.getEndMillis() - testResult.getStartMillis()) / 1000.0);
