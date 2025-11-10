3c2a62a8a0e78b0cc91e3dd09c8eb5870a24a35a
M-Ostapinskiyy <ostapinskiyy@gmail.com>
2025-11-10 20:23:21 +0300

    ConfigureAppearanceTest delete sleeps

--- src/test/java/school/redrover/ConfigureAppearanceTest.java
+++ src/test/java/school/redrover/ConfigureAppearanceTest.java
@@ -3,6 +3,7 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -45,7 +46,7 @@ public class ConfigureAppearanceTest extends BaseTest {
         getDriver().findElement(By.id("root-action-ManageJenkinsAction")).click();
         getDriver().findElement(By.cssSelector("a[href='appearance']")).click();
         getDriver().findElement(By.xpath("//label[contains(., 'Do not allow users to select a different theme')]")).click();
-        Thread.sleep(2000);
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.cssSelector("label:has(> div[data-theme='dark'])")));
         getDriver().findElement(By.cssSelector("label:has(> div[data-theme='dark'])")).click();
         getDriver().findElement(By.xpath("//label[contains(., 'Do not allow users to select a different theme')]")).click();
         getDriver().findElement(By.cssSelector("button.jenkins-submit-button")).click();
