9cd1363946e96839af8465e24488ea4b4c732cfe
Yana A <yanaalexandrovna94@gmail.com>
2025-11-11 22:27:19 +0200

    Added Create new Freestyle project test

---
+++ src/test/java/school/redrover/common/CreateNewItem7Test.java
@@ -0,0 +1,32 @@
+package school.redrover.common;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class CreateNewItem7Test extends BaseTest{
+
+    @Test
+    public void createNewFreeStyleProjectTest() {
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys("Freestyle Project Name");
+        getDriver().findElement(By.xpath("//span[text()='Freestyle project']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
+
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//h1"), "Freestyle Project"));
+
+        WebElement projectTitle = getDriver().findElement(By.xpath("//h1"));
+
+        Assert.assertEquals(projectTitle.getText(), "Freestyle Project Name");
+    }
+
+}
