7be817caab6451cc1d343fd750022ad70b2ce51d
kain4ra <kainara.idwsi@gmail.com>
2025-11-12 17:31:03 +0300

    add wait for click logo

--- src/test/java/school/redrover/page/ConfigurationFolderPage.java
+++ src/test/java/school/redrover/page/ConfigurationFolderPage.java
@@ -2,7 +2,6 @@ package school.redrover.page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import school.redrover.FolderSettingsTest;
 import school.redrover.common.BasePage;
 
 public class ConfigurationFolderPage extends BasePage {
