5001aa355a9e45697f08dd208eab2536bce4245b
pollindem <pollindem@gmail.com>
2025-11-12 18:38:35 +0200

    rename project via drop-down menu test

---
+++ src/test/java/school/redrover/MultiConfigProjectTest.java
@@ -0,0 +1,38 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class MultiConfigProjectTest extends BaseTest {
+
+    @Test
+    public void renameViaDropDownMenu() {
+
+        final String ProjectName = "New Project";
+        final String ChangedProjectName = "Multi-configuration project";
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(ProjectName);
+        getDriver().findElement(By.cssSelector("[class='hudson_matrix_MatrixProject']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
+
+        WebElement hoverElement = getDriver().findElement(By.cssSelector("[class$='hoverable-children-model-link']"));
+        Actions actions = new Actions(getDriver());
+        actions.moveToElement(hoverElement).perform();
+        getDriver().findElement(By.cssSelector("[href$='confirm-rename']")).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.cssSelector("[href$='confirm-rename']"))).click();
+
+        WebElement renameField = getDriver().findElement(By.name("newName"));
+        renameField.clear();
+        renameField.sendKeys(ChangedProjectName);
+        getDriver().findElement(By.xpath("//button[@name=\"Submit\"]")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.tagName("h1")).getText(), ChangedProjectName);
+    }
+}
