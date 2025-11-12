99a2db540dcd97402bf63bbcd6138ebcb724bf96
kain4ra <kainara.idwsi@gmail.com>
2025-11-12 15:01:07 +0300

    AT_07.001.01 | Organization folder Configuration > Change General Settings > Check Display name

---
+++ src/test/java/school/redrover/FolderSettingsTest.java
@@ -0,0 +1,32 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class FolderSettingsTest extends BaseTest {
+
+    private static String FOLDER_NAME = "Just folder";
+    private static String DISPLAY_NAME = "Look at this";
+
+    @Test
+    public void testDisplayName() {
+        getDriver().findElement(By.cssSelector("[href='newJob']")).click();
+
+        getDriver().findElement(By.name("name")).sendKeys(FOLDER_NAME);
+        getDriver().findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder")).click();
+        getDriver().findElement(By.cssSelector("button[type='submit']")).click();
+
+        getDriver().findElement(By.name("_.displayNameOrNull")).sendKeys(DISPLAY_NAME);
+        getDriver().findElement(By.name("Submit")).click();
+
+        getWait5().until(ExpectedConditions.elementToBeClickable(
+                getDriver().findElement(By.className("app-jenkins-logo")))).click();
+
+        WebElement project = getDriver().findElement(By.xpath("//tr[@id='job_%s']/td[3]".formatted(FOLDER_NAME)));
+        Assert.assertEquals(project.getText(), DISPLAY_NAME);
+    }
+}
