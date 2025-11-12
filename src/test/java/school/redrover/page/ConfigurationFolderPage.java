914a0c55afb633729a25842b41f2aa7e0373d85d
Sergey.D <sergei.demyanenko@gmail.com>
2025-11-11 23:57:26 -0500

    Add POM example

---
+++ src/test/java/school/redrover/page/ConfigurationFolderPage.java
@@ -0,0 +1,11 @@
+package school.redrover.page;
+
+import org.openqa.selenium.WebDriver;
+import school.redrover.common.BasePage;
+
+public class ConfigurationFolderPage extends BasePage {
+
+    public ConfigurationFolderPage(WebDriver driver) {
+        super(driver);
+    }
+}
