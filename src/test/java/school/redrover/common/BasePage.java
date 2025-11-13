7c602788f5322ddcfbc5cb88e3e815a20a9cede4
Alexey Kulagin <alexeyk@myhub24.com>
2025-11-12 21:14:57 -0800

    RF_15.001.02 | testCreateUser (#1098)
    https://github.com/RedRoverSchool/JenkinsQA_Java_2025_fall/issues/1096

--- src/test/java/school/redrover/common/BasePage.java
+++ src/test/java/school/redrover/common/BasePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.page.HomePage;
+import school.redrover.page.ManageJenkinsPage;
 
 public abstract class BasePage extends BaseModel {
 
@@ -15,5 +16,11 @@ public abstract class BasePage extends BaseModel {
         getWait5().until(ExpectedConditions.elementToBeClickable(By.className("app-jenkins-logo"))).click();
 
         return new HomePage(getDriver());
+    }
+
+    public ManageJenkinsPage clickGearManageJenkinsButton() {
+        getDriver().findElement(By.id("root-action-ManageJenkinsAction")).click();
+
+        return new ManageJenkinsPage(getDriver());
     }
 }
