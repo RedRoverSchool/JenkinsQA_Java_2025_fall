b72012c0c5e5aef8d96706c469179d0d868eb0ad
kiramitt <kiramitt@gmail.com>
2025-11-13 16:58:34 +0300

    RF_01.002.05.02 | New Item > Folder > testNewFolderDefaultAddedToExistingFolder >POM

--- src/test/java/school/redrover/page/FolderPage.java
+++ src/test/java/school/redrover/page/FolderPage.java
@@ -1,11 +1,35 @@
 package school.redrover.page;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.common.BasePage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FolderPage extends BasePage {
 
     public FolderPage(WebDriver driver) {
         super(driver);
+    }
+
+    public NewItemPage clickNewItem() {
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+        return new NewItemPage(getDriver());
+    }
+
+    public List<String> getBreadcrumbTexts() {
+        List<WebElement> breadcrumbElements = getWait2().until(
+                ExpectedConditions.visibilityOfAllElementsLocatedBy(
+                        By.xpath("//ol[@id='breadcrumbs']/li/a")));
+
+        List<String> breadcrumbTexts = new ArrayList<>();
+        for (WebElement element : breadcrumbElements) {
+            breadcrumbTexts.add(element.getText());
+        }
+
+        return breadcrumbTexts;
     }
 }
