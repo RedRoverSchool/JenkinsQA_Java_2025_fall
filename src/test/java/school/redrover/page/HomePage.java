52607909147af586d1cfdf8bfb3cb4d334a2278c
Kain <117358146+Kain4ra@users.noreply.github.com>
2025-11-13 12:51:23 +0300

    Update HomePage.java

--- src/test/java/school/redrover/page/HomePage.java
+++ src/test/java/school/redrover/page/HomePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import school.redrover.common.BasePage;
+import school.redrover.common.TestUtils;
 
 import java.util.List;
 
