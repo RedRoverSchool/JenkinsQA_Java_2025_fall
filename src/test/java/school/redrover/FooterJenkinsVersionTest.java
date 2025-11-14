a16155390265d5263b4665adb5b5d487a763685c
kain4ra <kainara.idwsi@gmail.com>
2025-11-14 18:46:57 +0300

    delete unused imports

--- src/test/java/school/redrover/FooterJenkinsVersionTest.java
+++ src/test/java/school/redrover/FooterJenkinsVersionTest.java
@@ -1,18 +1,10 @@
 package school.redrover;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import school.redrover.common.BasePage;
 import school.redrover.common.BaseTest;
 import school.redrover.page.HomePage;
 
-import java.time.Duration;
 import java.util.List;
 
 public class FooterJenkinsVersionTest extends BaseTest {
