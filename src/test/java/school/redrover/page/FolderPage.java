44f3450363c2c6900355c77c75355248bada3db8
aslanbekturganbayev <aslanbeksmailbox@gmail.com>
2025-11-13 23:03:37 -0500

    POM tests

--- src/test/java/school/redrover/page/FolderPage.java
+++ src/test/java/school/redrover/page/FolderPage.java
@@ -61,4 +61,27 @@ public class FolderPage extends BasePage {
 
         return this;
     }
+
+    public String getFolderContext() {
+        return getWait2()
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[text()='This folder is empty']")))
+                .getText();
+    }
+
+    public NewItemPage clickCreateJob() {
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+
+        return new NewItemPage(getDriver());
+    }
+
+    public NewItemPage clickNewItem() {
+        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.tagName("h1")));
+        getDriver().findElement(By.xpath("//span[text()='New Item']/..")).click();
+
+        return new NewItemPage(getDriver());
+    }
+
+    public WebElement getElement(String name){
+        return getDriver().findElement(By.xpath("//span[text()='%s']".formatted(name)));
+    }
 }
