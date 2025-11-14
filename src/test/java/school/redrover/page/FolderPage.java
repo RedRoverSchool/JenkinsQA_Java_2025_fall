adbbbdd848c3b039aee97141283e9e0cdb1eb24d
aslanbekturganbayev <aslanbeksmailbox@gmail.com>
2025-11-13 23:42:41 -0500

    POM tests

--- src/test/java/school/redrover/page/FolderPage.java
+++ src/test/java/school/redrover/page/FolderPage.java
@@ -69,7 +69,7 @@ public class FolderPage extends BasePage {
     }
 
     public NewItemPage clickCreateJob() {
-        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='newJob']"))).click();
 
         return new NewItemPage(getDriver());
     }
