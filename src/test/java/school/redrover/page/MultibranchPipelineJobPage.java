218625b9f4d70dfa4988fe5984a31043eac58db4
StepanidaKirillina1 <105031048+StepanidaKirillina1@users.noreply.github.com>
2025-11-14 17:51:06 +0900

    RF_06.015.03 | MultibranchPipelineConfigurationTest > testDisabledMessageOnStatusPage (#1157)

--- src/test/java/school/redrover/page/MultibranchPipelineJobPage.java
+++ src/test/java/school/redrover/page/MultibranchPipelineJobPage.java
@@ -27,4 +27,8 @@ public class MultibranchPipelineJobPage extends BasePage {
     public String getDescription() {
         return getDriver().findElement(By.id("view-message")).getText();
     }
+
+    public String getDisabledText() {
+        return getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.id("disabled-message"))).getText();
+    }
 }
