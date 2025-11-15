f440fef4ea73515920e64a6fb4ab115aa1f5270c
Yuliya Bezlatneva <bezlatneva@yandex.com>
2025-11-16 02:26:14 +0300

    refactor: testWelcomeMessageDisplayed to POM

--- src/test/java/school/redrover/WelcomeDashboardTest.java
+++ src/test/java/school/redrover/WelcomeDashboardTest.java
@@ -1,18 +1,17 @@
 package school.redrover;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
+import school.redrover.page.LoginPage;
 
 public class WelcomeDashboardTest extends BaseTest {
 
     @Test
     public void testWelcomeMessageDisplayed() {
-        WebElement welcomeMessage = getDriver().findElement(By.xpath("//div[@class='empty-state-block']/h1"));
+        String welcomeMessage = new LoginPage(getDriver())
+                .getTitle();
 
-        Assert.assertTrue(welcomeMessage.isDisplayed());
-        Assert.assertEquals(welcomeMessage.getText(), "Welcome to Jenkins!");
+        Assert.assertEquals(welcomeMessage, "Welcome to Jenkins!");
     }
 }
