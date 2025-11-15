37d40077c7b07f22952c080f2f792faeba8325b3
AlexeyK <AlexeyK@myhub24.com>
2025-11-15 13:09:45 -0800

    changed getWait2() to getWait5()

--- src/test/java/school/redrover/page/UserAccountPage.java
+++ src/test/java/school/redrover/page/UserAccountPage.java
@@ -14,7 +14,7 @@ public class UserAccountPage extends BasePage {
 
     public String getUserName() {
 
-        return getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='jenkins-app-bar']//h1")))
+        return getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='jenkins-app-bar']//h1")))
                 .getText();
     }
 }
