bf6c729d88624df99501ce98fd94645c7ddf8555
Yuliya Bezlatneva <bezlatneva@yandex.com>
2025-11-16 14:06:02 +0300

    refactor: testWelcomeMessageDisplayed to POM

--- src/test/java/school/redrover/WelcomeDashboardTest.java
+++ src/test/java/school/redrover/WelcomeDashboardTest.java
@@ -3,14 +3,14 @@ package school.redrover;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
-import school.redrover.page.LoginPage;
+import school.redrover.page.HomePage;
 
 public class WelcomeDashboardTest extends BaseTest {
 
     @Test
     public void testWelcomeMessageDisplayed() {
-        String welcomeMessage = new LoginPage(getDriver())
-                .getTitle();
+        String welcomeMessage = new HomePage(getDriver())
+                .getHeadingText();
 
         Assert.assertEquals(welcomeMessage, "Welcome to Jenkins!");
     }
