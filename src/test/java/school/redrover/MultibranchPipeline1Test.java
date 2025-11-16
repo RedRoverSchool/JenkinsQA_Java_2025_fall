957d725f628a51af06aaaec33186e4492ce64629
ISokolova <issklva@gmail.com>
2025-11-16 16:10:53 +0500

    RF_06.003.7 | Multibranch pipeline Configuration > Change Description #1263 Deleted class

--- src/test/java/school/redrover/MultibranchPipeline1Test.java
+++ src/test/java/school/redrover/MultibranchPipeline1Test.java
@@ -1,5 +1,6 @@
 package school.redrover;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
@@ -39,5 +40,24 @@ public class MultibranchPipeline1Test extends BaseTest {
                 .getText();
 
         Assert.assertEquals(name, MULTIBRANCH_PIPELINE_DISPLAY_NAME);
+    }
+
+
+    @Test
+    public void testChangeDescription() {
+
+        getDriver().findElement(By.cssSelector("a[href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys("New Multibranch Pipeline");
+        getDriver().findElement(By.xpath("//span[text()='Multibranch Pipeline']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.xpath("//textarea")).sendKeys("New test pipeline");
+        getDriver().findElement(By.name("Submit")).click();
+        getDriver().findElement(By.cssSelector("a[href='./configure']")).click();
+        getDriver().findElement(By.xpath("//textarea")).clear();
+        getDriver().findElement(By.xpath("//textarea")).sendKeys("Change description - New test pipeline 2");
+        getDriver().findElement(By.name("Submit")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.id("view-message")).getText(), "Change description - New test pipeline 2");
+
     }
 }
