e10eff1017fb52690c1fd0c74991c2083f555ce1
Stanislav Agafonov <chaltstas@yandex.ru>
2025-11-19 12:07:11 +0300

    RF_03.002.04.3 Удалил тесты  testCreatePipeline и testDeletePipeline

--- src/test/java/school/redrover/PipelineTest.java
+++ src/test/java/school/redrover/PipelineTest.java
@@ -120,40 +120,6 @@ public class PipelineTest extends BaseTest {
     }
 
     @Test
-    public void testCreatePipeline() {
-        getDriver().findElement(By.cssSelector(".task:nth-child(1) a")).click();
-        getDriver().findElement(By.cssSelector("#name")).sendKeys(PIPELINE_NAME);
-        getDriver().findElement(By.cssSelector("div:first-child > ul > li:nth-child(2)")).click();
-        getDriver().findElement(By.id("ok-button")).click();
-        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
-
-
-        getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/']/img"))).click();
-
-        Assert.assertEquals(getDriver().findElement(By.xpath("//a[@href='job/" + PIPELINE_NAME + "/']")).getText(),
-                PIPELINE_NAME);
-    }
-
-    @Test
-    public void testDeletePipeline() {
-        getDriver().findElement(By.cssSelector(".task:nth-child(1) a")).click();
-        getDriver().findElement(By.cssSelector("#name")).sendKeys(PIPELINE_NAME);
-        getDriver().findElement(By.cssSelector("div:first-child > ul > li:nth-child(2)")).click();
-        getDriver().findElement(By.id("ok-button")).click();
-        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
-
-        getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/']/img"))).click();
-
-        List<WebElement> countPosition = getDriver().findElements(By.cssSelector("#projectstatus > tbody > tr"));
-
-        getDriver().findElement(By.xpath("//a[@href='job/" + PIPELINE_NAME + "/']")).click();
-        getDriver().findElement(By.cssSelector(".task:nth-child(6)")).click();
-        getDriver().findElement(By.xpath("//button[@data-id='ok']")).click();
-
-        Assert.assertEquals(countPosition.size() - 1, 0);
-    }
-
-    @Test
     public void testSuccessfulBuildPipeline() {
         createPipeline(PIPELINE_NAME);
 
