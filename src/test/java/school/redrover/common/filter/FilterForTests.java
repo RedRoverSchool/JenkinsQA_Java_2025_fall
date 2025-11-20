0ce84612e13ae0e0e7043c8ed5f92df64d049406
tstyev <tst.yev@gmail.com>
2025-11-21 00:55:54 +0300

    Refactoring unit test

--- src/test/java/school/redrover/common/filter/FilterForTests.java
+++ src/test/java/school/redrover/common/filter/FilterForTests.java
@@ -1,22 +1,22 @@
 package school.redrover.common.filter;
 
-import org.testng.*;
+import org.testng.IMethodInstance;
+import org.testng.IMethodInterceptor;
+import org.testng.ITestContext;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class FilterForTests implements IMethodInterceptor {
 
     @Override
     public List<IMethodInstance> intercept(List<IMethodInstance> methods, ITestContext context) {
         String files = System.getenv("LIST_OF_CHANGED_FILES");
-        if (files != null) {
+        String dependenciesClasses = System.getenv("LIST_OF_DEPENDENCIES_CLASSES");
+        if (files != null && dependenciesClasses != null) {
             return FilterUtils.filterMethods(
                     Arrays.stream(files.split(";")).toList(),
+                    dependenciesClasses,
                     methods);
         }
 
