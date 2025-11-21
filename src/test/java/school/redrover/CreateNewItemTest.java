dc9aff579d3547e4495fff6f6f7e58b708a6c50c
evgenii-cher <evgg90@gmail.com>
2025-11-21 07:14:54 +0700

    test CopyFromItem from class CopyFromItemTest move to class CreateNewItemTest. Method rename to testNewItemByCopyingFromExistingItem. (#1412)
    Delete class CopyFromItemTest.

--- src/test/java/school/redrover/CreateNewItemTest.java
+++ src/test/java/school/redrover/CreateNewItemTest.java
@@ -8,6 +8,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.openqa.selenium.JavascriptExecutor;
 import school.redrover.common.BaseTest;
+import school.redrover.page.FreestyleProjectPage;
 import school.redrover.page.HomePage;
 import school.redrover.page.NewItemPage;
 
@@ -232,6 +233,40 @@ public class CreateNewItemTest extends BaseTest {
                 .get(0);
 
         Assert.assertEquals(jobName, newProject);
+    }
+
+    @Test
+    public void testNewItemByCopyingFromExistingItem() {
+        final String originalName = "originalProject";
+        final String newName = "newProject";
+        final String descriptionText = "RedRover is the best";
+        final String daysToKeepText = "10";
+        final String maxOfBuildsText = "20";
+        final String gitHubUrl = "git.com/testUrl";
+        final String buildsUrl = "example.com/test";
+
+        List <String> originalItemSettingsList =
+                List.of(descriptionText, daysToKeepText, maxOfBuildsText, gitHubUrl, buildsUrl);
+
+        List<String> copiedItemSettingsList = new HomePage(getDriver())
+                .clickCreateJob()
+                .sendName(originalName)
+                .selectFreestyleProjectAndSubmit()
+                .setDescription(descriptionText)
+                .setCheckBoxDiscardAndSetDaysNum(daysToKeepText, maxOfBuildsText)
+                .setCheckBoxGitHubAndSendUrl(gitHubUrl)
+                .setCheckBoxTriggerBuildsAndSendUrl(buildsUrl)
+                .clickSave()
+                .gotoHomePage()
+                .clickNewItemOnLeftMenu()
+                .sendName(newName)
+                .sendNameToCopyFromAndSubmit(originalName)
+                .gotoHomePage()
+                .openJobPage(newName, new FreestyleProjectPage(getDriver()))
+                .clickConfigure(newName)
+                .getSettingsToList();
+
+        Assert.assertEquals(originalItemSettingsList, copiedItemSettingsList);
     }
 }
 
