7ed437dafe01865410f2cec12869b30cffcf24e7
Sensation3000 <116028836+Sensation3000@users.noreply.github.com>
2025-11-24 15:16:53 +0300

    refactor TC_11.006.01 and TC_11.007.01 (#1490)

--- src/test/java/school/redrover/page/NewNodePage.java
+++ src/test/java/school/redrover/page/NewNodePage.java
@@ -2,6 +2,7 @@ package school.redrover.page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import school.redrover.common.BasePage;
 
 public class NewNodePage extends BasePage {
@@ -14,5 +15,13 @@ public class NewNodePage extends BasePage {
         getDriver().findElement(By.xpath("//a[@href='api/']")).click();
 
         return new RestApiPage(getDriver());
+    }
+
+    public String getHeadingText() {
+        return getDriver().findElement(By.xpath("//h1")).getText();
+    }
+
+    public boolean isFormDisplayed() {
+        return getDriver().findElement(By.xpath("//form")).isDisplayed();
     }
 }
