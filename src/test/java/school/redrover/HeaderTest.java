c1d472c5e076e537ababd892c9352bed2566c983
gaitanchristina92 <gaitanchristina@gmail.com>
2025-11-24 19:38:06 +0300

    AT_13.002.03 Header > Access Global Search> Navigation through Search Results

--- src/test/java/school/redrover/HeaderTest.java
+++ src/test/java/school/redrover/HeaderTest.java
@@ -1,14 +1,17 @@
 package school.redrover;
 
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import school.redrover.common.BasePage;
 import school.redrover.common.BaseTest;
-import school.redrover.page.FolderPage;
-import school.redrover.page.HomePage;
+import school.redrover.page.*;
 
+import java.io.FileInputStream;
 import java.util.List;
+import java.util.Properties;
 
 public class HeaderTest extends BaseTest {
 
@@ -31,6 +34,7 @@ public class HeaderTest extends BaseTest {
 
     @Test(dependsOnMethods = "testSearchResultsAppear")
     public void testSearchResultsActions() {
+
         Actions actions = new Actions(getDriver());
         new HomePage(getDriver()).clickSearchButton().searchFor("Folder").waitForTextOfResults();
 
@@ -39,5 +43,57 @@ public class HeaderTest extends BaseTest {
         actions.sendKeys(Keys.ENTER).perform();
 
         Assert.assertTrue(new FolderPage(getDriver()).checkURLContains("/job/Folder/"));
+    }
+
+    @Test
+    public void testGlobalSearchButtonAppears() throws InterruptedException {
+        Navigation navigation = new Navigation();
+        List<BasePage> mainJenkinsPages = List.of(
+                navigation.goToNewItem(),
+                navigation.goToManageJenkins(),
+                navigation.goToUserPage(),
+                navigation.goToBuildHistory()
+        );
+        for (BasePage page : mainJenkinsPages) {
+            Assert.assertTrue(page.isSearchButtonPresent());
+            page.gotoHomePage();
+        }
+    }
+
+    class Navigation {
+        static private final Properties properties = new Properties();
+
+        static {
+            try {
+                FileInputStream file = new FileInputStream("src/test/resources/.properties");
+                properties.load(file);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private String getBaseURL() {
+            return properties.getProperty("jenkins.host") + ":" + properties.getProperty("jenkins.port");
+        }
+
+        public NewItemPage goToNewItem() {
+            new HomePage(getDriver()).clickNewItemOnLeftMenu();
+            return new NewItemPage(getDriver());
+        }
+
+        public BuildHistoryOfJenkinsPage goToBuildHistory() {
+            new HomePage(getDriver()).clickBuildHistoryWithGetWait();
+            return new BuildHistoryOfJenkinsPage(getDriver());
+        }
+
+        public ManageJenkinsPage goToManageJenkins() {
+            new HomePage(getDriver()).clickManageJenkinsIcon();
+            return new ManageJenkinsPage(getDriver());
+        }
+
+        public ManageUsersPage goToUserPage() {
+            new HomePage(getDriver()).clickUserAccount();
+            return new ManageUsersPage(getDriver());
+        }
     }
 }
