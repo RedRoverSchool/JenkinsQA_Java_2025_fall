2a29de570d2f6fdf8fb3bd9ab96499608a640b39
Adelya Sensation <omsksmp@gmail.com>
2025-11-24 13:39:33 +0300

    RF_11.004.01

--- src/test/java/school/redrover/page/BuildHistoryOfJenkinsPage.java
+++ src/test/java/school/redrover/page/BuildHistoryOfJenkinsPage.java
@@ -2,7 +2,11 @@ package school.redrover.page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import school.redrover.common.BasePage;
+
+import java.util.List;
 
 public class BuildHistoryOfJenkinsPage extends BasePage {
 
@@ -10,9 +14,27 @@ public class BuildHistoryOfJenkinsPage extends BasePage {
         super(driver);
     }
 
-    public RestApiPage clickRestApiLink(){
+    public RestApiPage clickRestApiLink() {
         getDriver().findElement(By.xpath("//a[@href='api/']")).click();
 
         return new RestApiPage(getDriver());
+    }
+
+    public boolean emptyTableIsDisplayed() {
+        return getDriver().findElement(By.xpath("//*[@id='projectStatus']")).isDisplayed();
+    }
+
+    public String getHeadingText() {
+        return getDriver().findElement(By.tagName("h1")).getText();
+    }
+
+    public List<String> getTableHeadersText() {
+        return getDriver()
+                .findElements(By.cssSelector("thead th a.sortheader"))
+                .stream()
+                .map(webElement -> webElement.getText()
+                        .replaceAll("[↓↑\\s\\u00A0]+$", "")
+                        .trim())
+                .toList();
     }
 }
