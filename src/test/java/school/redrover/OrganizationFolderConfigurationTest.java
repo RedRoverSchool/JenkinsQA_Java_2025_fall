b7a54a3b50cfcd33f4add3df5ef25f7b4dfec40b
Vlas Mastykash <mastykash.vlas@gmail.com>
2025-11-26 23:37:24 +0600

    #1567: AT_07.001.07 | Organization folder Configuration > Tooltip has link to Jenkins website (#1569)

---
+++ src/test/java/school/redrover/OrganizationFolderConfigurationTest.java
@@ -0,0 +1,46 @@
+package school.redrover;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+import school.redrover.page.HomePage;
+import school.redrover.page.OrganizationFolderPage;
+
+public class OrganizationFolderConfigurationTest extends BaseTest {
+
+    private static final String FOLDER_NAME = "Organization Folder %d".formatted(System.currentTimeMillis());
+
+    @Test
+    public void testAddDisplayNameAndDescription() {
+        String expectedDisplayName = "New folder name";
+        String expectedDescription = "New Description";
+
+        OrganizationFolderPage organizationFolderPage = new HomePage(getDriver())
+                .clickNewItemOnLeftMenu()
+                .sendName(FOLDER_NAME)
+                .selectOrganizationFolderAndSubmit()
+                .inputDisplayName(expectedDisplayName)
+                .inputDescription(expectedDescription)
+                .clickSave();
+
+        String actualDisplayName = organizationFolderPage.getDisplayNameOrganizationFolder();
+        String actualDescription = organizationFolderPage.getDescriptionOrganizationFolder();
+
+        Assert.assertEquals(actualDisplayName, expectedDisplayName);
+        Assert.assertEquals(actualDescription, expectedDescription);
+    }
+
+    @Test
+    public void testDisplayNameTooltipLink() {
+        String expectedExternalLink = "https://plugins.jenkins.io/branch-api";
+
+        String displayNameTooltipLink = new HomePage(getDriver())
+                .clickCreateJob()
+                .sendName(FOLDER_NAME)
+                .selectOrganizationFolderAndSubmit()
+                .clickDisplayNameTooltip()
+                .getDisplayNameTooltipLink();
+
+        Assert.assertEquals(displayNameTooltipLink, expectedExternalLink);
+    }
+}
