c2a96b4231ee5e1ed6067240b7e1ae0d6364279c
Nat P <natashaadam.p43@gmail.com>
2025-11-27 14:44:03 +0100

    #rf-findby-to-folderconfig
    added @FindBy to POM FolderConfigurationPage

--- src/test/java/school/redrover/FolderConfigurationTest.java
+++ src/test/java/school/redrover/FolderConfigurationTest.java
@@ -6,7 +6,6 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 import school.redrover.page.*;
-
 import java.util.Arrays;
 import java.util.List;
 
