94af612dd1b5db2b8a1411f9161c63a8207a5d65
kirillbokhenek <kirill.bohenek@gmail.com>
2025-11-27 21:40:41 +0200

    TC_10.012.012| Manage Jenkins > Configure Appearance > User Themes Prism Stages and Graph + delete old test without pom new new methods again

--- src/test/java/school/redrover/ConfigureAppearanceTest.java
+++ src/test/java/school/redrover/ConfigureAppearanceTest.java
@@ -3,14 +3,12 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
+
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
-import school.redrover.page.AppearancePage;
 import school.redrover.page.HomePage;
 
 import java.time.Duration;
