1b00b50234c16a6f77ddffeb363dc7e1c10a25c4
xkaktyzx <xkaktyzx1@gmail.com>
2025-11-28 19:33:13 +0100

    AT_19.012.01 | Pipeline Management > View Pipeline Syntax Documentation > Button is available in the side menu
    Added FindBy to page classes

--- src/test/java/school/redrover/page/PipelineSyntaxPage.java
+++ src/test/java/school/redrover/page/PipelineSyntaxPage.java
@@ -1,11 +1,8 @@
 package school.redrover.page;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.FindBys;
 import school.redrover.common.BasePage;
 
 import java.util.List;
