cc2a926512fcc544d6ffe8a36f55a2940974ebc9
kirillbokhenek <kirill.bohenek@gmail.com>
2025-11-28 19:45:40 +0200

    TC_10.012.012| Manage Jenkins > Configure Appearance > User Themes Prism Stages and Graph + delete old test without pom new new methods again

--- src/test/java/school/redrover/page/AppearancePage.java
+++ src/test/java/school/redrover/page/AppearancePage.java
@@ -52,17 +52,18 @@ public class AppearancePage extends BasePage {
     }
 
     public AppearancePage changePrismSyntaxHighlightingThemeDefault() {
-        new Select(getWait10().until(ExpectedConditions.elementToBeClickable(By.name("_.theme")))).selectByValue("PRISM");
+
+        new Select(getWait10().until(ExpectedConditions.presenceOfElementLocated(By.name("_.theme")))).selectByValue("PRISM");
         return this;
     }
 
     public AppearancePage changePrismSyntaxHighlightingThemeCoy() {
-        new Select(getWait10().until(ExpectedConditions.elementToBeClickable(By.name("_.theme")))).selectByValue("COY");
+        new Select(getWait10().until(ExpectedConditions.presenceOfElementLocated(By.name("_.theme")))).selectByValue("COY");
         return this;
     }
 
     public AppearancePage changePrismSyntaxHighlightingDark() {
-        new Select(getWait10().until(ExpectedConditions.elementToBeClickable(By.name("_.theme")))).selectByValue("DARK");
+        new Select(getWait10().until(ExpectedConditions.presenceOfElementLocated(By.name("_.theme")))).selectByValue("DARK");
         return this;
     }
 }
