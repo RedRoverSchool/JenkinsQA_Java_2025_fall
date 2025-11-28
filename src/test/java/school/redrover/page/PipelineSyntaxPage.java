e8ad260d5b6178b153cbd3291dc6fd41c74ae19d
xkaktyzx <xkaktyzx1@gmail.com>
2025-11-28 19:32:25 +0100

    AT_19.012.01 | Pipeline Management > View Pipeline Syntax Documentation > Button is available in the side menu
    Added FindBy to page classes

--- src/test/java/school/redrover/page/PipelineSyntaxPage.java
+++ src/test/java/school/redrover/page/PipelineSyntaxPage.java
@@ -3,11 +3,17 @@ package school.redrover.page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
 import school.redrover.common.BasePage;
 
 import java.util.List;
 
 public class PipelineSyntaxPage extends BasePage {
+
+    @FindBy(className = "task")
+    private List<WebElement> sideMenuButtons;
 
     public PipelineSyntaxPage(WebDriver driver) {
         super(driver);
@@ -15,8 +21,7 @@ public class PipelineSyntaxPage extends BasePage {
 
     public List<String> getListOfButtonsInSideMenu() {
 
-        return getDriver()
-                .findElements(By.className("task"))
+        return sideMenuButtons
                 .stream()
                 .map(WebElement::getText)
                 .toList();
