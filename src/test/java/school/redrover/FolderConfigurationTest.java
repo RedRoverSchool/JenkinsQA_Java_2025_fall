0423e881a558be7c4efdd8758f8b45395d7ab7bc
Elena-Galina <e.lena.galina@yandex.ru>
2025-11-29 16:35:47 +0300

    RF_21.007- Folder Management > Configure Job Item moved tests to FolderManagementTest

--- src/test/java/school/redrover/FolderConfigurationTest.java
+++ src/test/java/school/redrover/FolderConfigurationTest.java
@@ -1,8 +1,6 @@
 package school.redrover;
 
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 import school.redrover.page.*;
@@ -12,26 +10,6 @@ import java.util.List;
 public class FolderConfigurationTest extends BaseTest {
 
    private static final String FOLDER_NAME = "MyFolder";
-
-   @DataProvider
-   public Object[][] itemsData() {
-        return new Object[][]{
-                {"freestyle01", "Freestyle project"},
-                {"pipeline01", "Pipeline"},
-                {"multiConfig01", "Multi-configuration project"},
-                {"folder01", "Folder"},
-                {"Multibranch01", "Multibranch Pipeline"},
-                {"orgFolder01", "Organization Folder"}
-        };
-    }
-
-    public void createFolder() {
-        new HomePage(getDriver())
-                .clickCreateJob()
-                .sendName(FOLDER_NAME)
-                .selectFolderAndSubmit()
-                .gotoHomePage();
-    }
 
     @Test
     public void testHealthMetricLinkIsDisplayed(){
@@ -216,96 +194,5 @@ public class FolderConfigurationTest extends BaseTest {
                 .getRecursiveTooltipText();
 
         Assert.assertEquals(actualText, expectedTooltip);
-    }
-
-    @Test(dataProvider = "itemsData")
-    public void testConfigureMenuItemInDropdownForEachJob(String itemName, String itemType) {
-        final String menuItem = "Configure";
-        createFolder();
-
-        boolean isConfigureMenuItemDisplayed = new HomePage(getDriver())
-                .clickFolder(FOLDER_NAME)
-                .clickSidebarNewItem()
-                .sendName(itemName)
-                .selectItemTypeAndSubmitAndGoHome(itemType)
-                .clickFolder(FOLDER_NAME)
-                .openItemDropdownMenu(itemName)
-                .isMenuItemInDropdownDisplayed(menuItem);
-
-        Assert.assertTrue(isConfigureMenuItemDisplayed);
-    }
-
-    @Test
-    public void testNavigateToConfigurationViaSideMenuForPipeline() {
-        final String itemName = "pipeline01";
-
-        createFolder();
-
-        String actualHeadingText = new HomePage(getDriver())
-                .clickFolder(FOLDER_NAME)
-                .clickSidebarNewItem()
-                .sendName(itemName)
-                .selectPipelineAndSubmit()
-                .gotoHomePage()
-                .clickFolder(FOLDER_NAME)
-                .openItemPage(itemName, new PipelinePage(getDriver()))
-                .clickConfigureLinkInSideMenu()
-                .getHeadingText();
-
-        Assert.assertEquals(actualHeadingText, "Configure");
-    }
-
-    @Test(dataProvider = "itemsData")
-    public void testNavigateToConfigurationViaSideMenuForEachJob(String itemName, String itemType) {
-        final String expectedBreadcrumbItem = "Configuration";
-
-        createFolder();
-
-        new HomePage(getDriver())
-                .clickFolder(FOLDER_NAME)
-                .clickSidebarNewItem()
-                .sendName(itemName)
-                .selectItemTypeAndSubmitAndGoHome(itemType)
-                .clickFolder(FOLDER_NAME);
-
-        String actualBreadcrumbItem = getBreadcrumbItem(itemName, itemType);
-        Assert.assertEquals(actualBreadcrumbItem, expectedBreadcrumbItem);
-    }
-
-    private String getBreadcrumbItem(String itemName, String itemType) {
-        switch (itemType) {
-            case "Freestyle project":
-                return new FolderPage(getDriver())
-                        .openItemPage(itemName, new FreestyleProjectPage(getDriver()))
-                        .clickConfigureLinkInSideMenu()
-                        .getBreadcrumbItem();
-            case "Pipeline":
-                return new FolderPage(getDriver())
-                        .openItemPage(itemName, new PipelinePage(getDriver()))
-                        .clickConfigureLinkInSideMenu()
-                        .getBreadcrumbItem();
-            case "Multi-configuration project":
-                return new FolderPage(getDriver())
-                        .openItemPage(itemName, new MultiConfigurationProjectPage(getDriver()))
-                        .clickConfigureLinkInSideMenu()
-                        .getBreadcrumbItem();
-            case "Folder":
-                return new FolderPage(getDriver())
-                        .openItemPage(itemName, new FolderPage(getDriver()))
-                        .clickConfigureLinkInSideMenu()
-                        .getBreadcrumbItem();
-            case "Multibranch Pipeline":
-                return new FolderPage(getDriver())
-                        .openItemPage(itemName, new MultibranchPipelineProjectPage(getDriver()))
-                        .clickConfigureLinkInSideMenu()
-                        .getBreadcrumbItem();
-            case "Organization Folder":
-                return new FolderPage(getDriver())
-                        .openItemPage(itemName, new OrganizationFolderPage(getDriver()))
-                        .clickConfigureLinkInSideMenu()
-                        .getBreadcrumbItem();
-            default:
-                throw new IllegalArgumentException("Unknown item type: " + itemType);
-        }
     }
 }
