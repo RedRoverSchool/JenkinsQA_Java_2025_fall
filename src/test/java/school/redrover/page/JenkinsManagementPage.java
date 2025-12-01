a915c5754678da40a298dfa82e224ed0ad4d0941
Marin4kin <marinkatik1@gmail.com>
2025-12-01 21:46:26 +0300

    RF_сonfigureAppearanceTests FindBy

--- src/test/java/school/redrover/page/JenkinsManagementPage.java
+++ src/test/java/school/redrover/page/JenkinsManagementPage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.common.BasePage;
+
 import java.util.List;
 
 
