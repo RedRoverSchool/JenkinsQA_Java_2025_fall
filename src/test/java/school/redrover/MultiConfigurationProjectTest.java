2cfd5a162ced5add567530a89421d451fd797e2c
pollindem <pollindem@gmail.com>
2025-12-01 13:22:46 +0200

    RF_04.010.01: Refactor to POM

--- src/test/java/school/redrover/MultiConfigurationProjectTest.java
+++ src/test/java/school/redrover/MultiConfigurationProjectTest.java
@@ -19,56 +19,6 @@ public class MultiConfigurationProjectTest extends BaseTest {
     private static final String RENAMED_PROJECT = "Renamed multiconfiguration project";
     private static final String PROJECT_DESCRIPTION = "Project description...";
 
-    SoftAssert softAssert = new SoftAssert();
-
-    public WebDriverWait waitTime(int time) {
-        return new WebDriverWait(getDriver(), Duration.ofSeconds(time));
-    }
-
-    public void createNewJob() {
-        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
-    }
-
-    public void setNameOfProject(String nameOfProject) {
-        getDriver().findElement(By.name("name")).sendKeys(nameOfProject);
-    }
-
-    public void selectProject() {
-        getDriver().findElement(By.xpath("(//div[@id='j-add-item-type-standalone-projects']/ul)/li[3]"))
-                .click();
-    }
-
-    public void submitCreateProject() {
-        getDriver().findElement(By.id("ok-button")).click();
-    }
-
-    public void submitConfigure() {
-        getDriver().findElement(By.name("Submit")).click();
-    }
-
-    public void goToProject(String projectName) {
-        getDriver().findElement(By.xpath(String.format("//td/a[@href='job/%s/']", projectName))).click();
-    }
-
-    public void goToDashBoard() {
-        waitTime(30).until(ExpectedConditions.visibilityOfElementLocated((By.className("app-jenkins-logo"))))
-                .click();
-    }
-
-    public void editDescription(String text) {
-        getDriver().findElement(By.xpath("//a[@href='editDescription']"))
-                .click();
-        WebElement description = getDriver().findElement(By.name("description"));
-        description.clear();
-        description.sendKeys(text);
-        getDriver().findElement(By.name("Submit")).click();
-    }
-
-    public String checkDescription() {
-        return waitTime(40).until(ExpectedConditions.presenceOfElementLocated(By.id("description-content")))
-                .getText();
-    }
-
     @Test
     public void testCreateProject() {
         String actualProjectName = new HomePage(getDriver())
@@ -79,6 +29,20 @@ public class MultiConfigurationProjectTest extends BaseTest {
                 .getHeading();
 
         Assert.assertEquals(actualProjectName, PROJECT_NAME);
+    }
+
+    @Test()
+    public void testAddDescriptionToProject() {
+        String actualDescription = new HomePage(getDriver())
+                .clickSidebarNewItem()
+                .sendName(PROJECT_NAME)
+                .selectMultiConfigurationProjectAndSubmit()
+                .clickSubmit()
+                .clearDescriptionField()
+                .sendDescription(PROJECT_DESCRIPTION)
+                .getDescription();
+
+        Assert.assertEquals(actualDescription, PROJECT_DESCRIPTION);
     }
 
     @Test
@@ -94,22 +58,6 @@ public class MultiConfigurationProjectTest extends BaseTest {
                 .getHeading();
 
         Assert.assertEquals(actualProjectName, RENAMED_PROJECT);
-    }
-
-    @Ignore
-    @Test
-    public void testAddDescriptionToProject() {
-        createNewJob();
-        setNameOfProject(PROJECT_NAME);
-        selectProject();
-        submitCreateProject();
-        submitConfigure();
-        goToDashBoard();
-        goToProject(PROJECT_NAME);
-        editDescription(PROJECT_DESCRIPTION);
-        String result = checkDescription();
-
-        softAssert.assertEquals(result, PROJECT_DESCRIPTION);
     }
 
     @Test
