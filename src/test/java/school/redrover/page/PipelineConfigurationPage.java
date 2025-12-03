9d8eb2babc945e0d33699a7fcfa23dd6d2808c30
Lev Eydelkind <eydelkindl@gmail.com>
2025-12-03 14:07:56 +0200

    RF_ID: 03.003 | to POM > Pipeline Configuration > Build Triggers #1509

--- src/test/java/school/redrover/page/PipelineConfigurationPage.java
+++ src/test/java/school/redrover/page/PipelineConfigurationPage.java
@@ -161,7 +161,7 @@ public class PipelineConfigurationPage extends BasePage {
        WebElement trigger1 = getWait10().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input/following-sibling::label[text()='Build after other projects are built']")));
        WebElement trigger2 = getWait10().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input/following-sibling::label[text()='Build periodically']")));
        WebElement trigger3 = getWait10().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input/following-sibling::label[text()='GitHub hook trigger for GITScm polling']")));
-        WebElement trigger4 = getWait10().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input/following-sibling::label[text()='Poll SCM']")));
+       WebElement trigger4 = getWait10().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input/following-sibling::label[text()='Poll SCM']")));
        WebElement trigger5 = getWait10().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input/following-sibling::label[text()='Trigger builds remotely (e.g., from scripts)']")));
 
        WebElement[] triggers = {trigger1, trigger2, trigger3, trigger4, trigger5};
