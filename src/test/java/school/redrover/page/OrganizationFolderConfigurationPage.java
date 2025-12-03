4b6b18ceee2fa15c2e2f3626756e4a25555978c3
Vlas Mastykash <mastykash.vlas@gmail.com>
2025-12-03 23:26:47 +0600

    #1687: AT_07.001.08 | Organization folder Configuration > Repository Sources Filtered By Name Part

--- src/test/java/school/redrover/page/OrganizationFolderConfigurationPage.java
+++ src/test/java/school/redrover/page/OrganizationFolderConfigurationPage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.common.BasePage;
 
+import java.util.List;
+
 public class OrganizationFolderConfigurationPage extends BasePage {
 
     public OrganizationFolderConfigurationPage(WebDriver driver) {
@@ -21,13 +23,15 @@ public class OrganizationFolderConfigurationPage extends BasePage {
     }
 
     public OrganizationFolderConfigurationPage inputDisplayName(String name) {
-        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.name("_.displayNameOrNull"))).sendKeys(name);
+        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By
+                .name("_.displayNameOrNull"))).sendKeys(name);
 
         return this;
     }
 
     public OrganizationFolderConfigurationPage inputDescription(String name) {
-        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.name("_.description"))).sendKeys(name);
+        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By
+                .name("_.description"))).sendKeys(name);
 
         return this;
     }
@@ -47,5 +51,23 @@ public class OrganizationFolderConfigurationPage extends BasePage {
     public String getBreadcrumbItem() {
         return getWait10().until(ExpectedConditions.visibilityOfElementLocated(By
                 .xpath("//span[contains(text(),'Configuration')]"))).getText();
+    }
+
+    public OrganizationFolderConfigurationPage filterRepositorySources(String sourceName) {
+        getWait2().until(ExpectedConditions.elementToBeClickable(By
+                .cssSelector("button[suffix='navigators']"))).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(By
+                .cssSelector("input[placeholder='Filter']"))).sendKeys(sourceName);
+
+        return this;
+    }
+
+    public List<String> getRepositorySourceNames() {
+        return getWait2()
+                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By
+                        .cssSelector("button.jenkins-dropdown__item:not([style*='display: none'])")))
+                .stream()
+                .map(WebElement::getText)
+                .toList();
     }
 }
